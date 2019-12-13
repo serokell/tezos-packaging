211dec253debc7923f7e419b6c0c47142b5ec133
rvem <roman.melnikov@serokell.io>
2019-12-13 15:22:01 +0300

    [#14] Package all new binaries
    Problem: Now we build not only `tezos-client` but also other
    tezos-related executables.
    
    Solution: Pack them all in both `.rpm` and `.deb` packages.

--- default.nix
+++ default.nix
@@ -6,19 +6,21 @@ with pkgs;
 
 let
   root = ./.;
-  mainnet = rec {
+  protocol005 = rec {
+    protocolName = "005_PsBabyM1";
+    binarySuffix = builtins.replaceStrings [ "_" ] [ "-" ] protocolName;
+  };
+  mainnet = {
     rev = "94f779a7";
     sha256 = "16lxilng5q8fr2ll6h4hf7wlvac6nmw4cx10cbgzj5ks090bl97r";
     patchFile = ./nix/fix-mainnet.patch;
-    protoName = "005_PsBabyM1";
-    binarySuffix = builtins.replaceStrings ["_"] ["-"] protoName;
+    protocol = protocol005;
   };
-  babylonnet = rec {
+  babylonnet = {
     rev = "b8731913";
     sha256 = "1pakf1s6bg76fq42mb8fj1immz9g9wwimd522cpx8k28zf0hkl5i";
     patchFile = ./nix/fix-babylonnet.patch;
-    protoName = "005_PsBabyM1";
-    binarySuffix = builtins.replaceStrings ["_"] ["-"] protoName;
+    protocol = protocol005;
   };
   static-nix = import ./nix/static.nix;
   tezos-static-mainnet = static-nix mainnet;
@@ -39,59 +41,70 @@ let
   babylonnet-binaries =
     packDirectory "babylonnet-binaries-${babylonnet.rev}"
     "${tezos-static-babylonnet}/bin";
+  licenseFile = "${tezos-static-mainnet}/LICENSE";
 
-  binary-mainnet = "${tezos-static-mainnet}/bin/tezos-client";
-  binary-babylonnet = "${tezos-static-babylonnet}/bin/tezos-client";
-  packageDesc-mainnet = {
-    inherit licenseFile;
-    project = "tezos-client-mainnet";
-    version = toString timestamp;
-    bin = binary-mainnet;
-    arch = "amd64";
-    license = "MIT";
-    dependencies = "";
-    maintainer = "Serokell https://serokell.io";
-    description = "CLI client for interacting with tezos blockchain";
-    gitRevision = mainnet.rev;
-    branchName = "mainnet";
-  };
+  mkPackageDescs = { executableName, binPath, description }@executableDesc:
+    let
+      mainnetDesc = {
+        inherit description;
+        bin = "${tezos-static-mainnet}/${binPath}";
+        gitRevision = mainnet.rev;
+        project = "${executableName}-mainnet";
+        version = toString timestamp;
+        arch = "amd64";
+        license = "MPL-2.0";
+        dependencies = "";
+        maintainer = "Serokell https://serokell.io";
+        licenseFile = "${tezos-static-mainnet}/LICENSE";
+        branchName = "mainnet";
+      };
+      babylonnetDesc = mainnetDesc // {
+        bin = "${tezos-static-babylonnet}/${binPath}";
+        project = "${executableName}-babylonnet";
+        gitRevision = babylonnet.rev;
+        branchName = "babylonnet";
+      };
+    in [ mainnetDesc babylonnetDesc ];
 
-  packageDesc-babylonnet = packageDesc-mainnet // {
-    project = "tezos-client-babylonnet";
-    bin = binary-babylonnet;
-    gitRevision = babylonnet.rev;
-    branchName = "babylonnet";
-  };
+  tezos-executables = [
+    {
+      executableName = "tezos-client";
+      binPath = "/bin/tezos-client";
+      description = "CLI client for interacting with tezos blockchain";
+    }
+    {
+      executableName = "tezos-admin-client";
+      binPath = "/bin/tezos-admin-client";
+      description = "Administration tool for the node";
+    }
+    {
+      executableName = "tezos-node";
+      binPath = "/bin/tezos-node";
+      description =
+        "Entry point for initializing, configuring and running a Tezos node";
+    }
+    {
+      executableName = "tezos-baker";
+      binPath = "/bin/tezos-baker-${protocol005.binarySuffix}";
+      description = "Daemon for baking";
+    }
+    {
+      executableName = "tezos-accuser";
+      binPath = "/bin/tezos-accuser-${protocol005.binarySuffix}";
+      description = "Daemon for accusing";
+    }
+    {
+      executableName = "tezos-endorser";
+      binPath = "/bin/tezos-endorser-${protocol005.binarySuffix}";
+      description = "Daemon for endorsing";
+    }
+    {
+      executableName = "tezos-signer";
+      binPath = "/bin/tezos-signer";
+      description = "A client to remotely sign operations or blocks";
+    }
+  ];
 
-  buildDeb = import ./packageDeb.nix { inherit stdenv writeTextFile dpkg; };
-  buildRpm = packageDesc:
-  import ./packageRpm.nix { inherit stdenv writeTextFile gnutar rpm buildFHSUserEnv; }
-    (packageDesc // { arch = "x86_64"; });
-
-  mainnet-rpm-package = buildRpm packageDesc-mainnet;
-
-  mainnet-deb-package = buildDeb packageDesc-mainnet;
-
-  babylonnet-rpm-package = buildRpm packageDesc-babylonnet;
-
-  babylonnet-deb-package = buildDeb packageDesc-babylonnet;
-
-  tezos-client-mainnet = stdenv.mkDerivation rec {
-    name = "tezos-client-mainnet-${mainnet.rev}";
-    phases = "copyPhase";
-    copyPhase = ''
-      mkdir -p $out
-      cp ${binary-mainnet} $out/${name}
-    '';
-  };
-  tezos-client-babylonnet = stdenv.mkDerivation rec {
-    name = "tezos-client-babylonnet-${babylonnet.rev}";
-    phases = "copyPhase";
-    copyPhase = ''
-      mkdir -p $out
-      cp ${binary-babylonnet} $out/${name}
-    '';
-  };
   tezos-license = stdenv.mkDerivation rec {
     name = "LICENSE";
     phases = "copyPhase";
@@ -101,8 +114,29 @@ let
     '';
   };
 
+  packageDescs = lib.flatten (map mkPackageDescs tezos-executables);
+
+  buildDeb = import ./packageDeb.nix { inherit stdenv writeTextFile dpkg; };
+  buildRpm = packageDesc:
+    import ./packageRpm.nix {
+      inherit stdenv writeTextFile gnutar rpm buildFHSUserEnv;
+    } (packageDesc // { arch = "x86_64"; });
+
+  moveDerivations = name: drvs:
+    stdenv.mkDerivation rec {
+      inherit name drvs;
+
+      buildCommand = ''
+        mkdir -p $out
+        for drv in $drvs; do
+          cp $drv/* $out
+        done
+      '';
+    };
+
+  deb-packages = moveDerivations "deb-packages" (map buildDeb packageDescs);
+  rpm-packages = moveDerivations "rpm-packages" (map buildRpm packageDescs);
+
 in rec {
-  inherit
-    mainnet-rpm-package babylonnet-rpm-package babylonnet-deb-package
-    mainnet-binaries babylonnet-binaries;
+  inherit deb-packages rpm-packages mainnet-binaries babylonnet-binaries tezos-license;
 }

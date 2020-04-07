78a0c54a9e272aa21e937503dd309ff21ca9f433
Alexander Bantyev <balsoft@balsoft.ru>
2020-04-07 22:17:31 +0300

    Switch to opam-nix
    Co-authored-by: Roman Melnikov <rvembox@gmail.com>

---
+++ release-binaries.nix
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: 2019 TQ Tezos <https://tqtezos.com/>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+let
+  protocols = import ./protocols.nix;
+  protocolsFormatted =
+    builtins.concatStringsSep ", " (protocols.allowed ++ protocols.active);
+in [
+  {
+    name = "tezos-client";
+    description = "CLI client for interacting with tezos blockchain";
+    supports = protocolsFormatted;
+  }
+  {
+    name = "tezos-admin-client";
+    description = "Administration tool for the node";
+    supports = protocolsFormatted;
+  }
+  {
+    name = "tezos-node";
+    description =
+      "Entry point for initializing, configuring and running a Tezos node";
+    supports = protocolsFormatted;
+  }
+  {
+    name = "tezos-signer";
+    description = "A client to remotely sign operations or blocks";
+    supports = protocolsFormatted;
+  }
+] ++ builtins.concatMap (protocol: [
+  {
+    name = "tezos-baker-${protocol}";
+    description = "Daemon for baking";
+    supports = protocol;
+  }
+  {
+    name = "tezos-accuser-${protocol}";
+    description = "Daemon for accusing";
+    supports = protocol;
+  }
+  {
+    name = "tezos-endorser-${protocol}";
+    description = "Daemon for endorsing";
+    supports = protocol;
+  }
+]) protocols.active
--- release.nix
+++ release.nix
@@ -1,22 +1,33 @@
 # SPDX-FileCopyrightText: 2019 TQ Tezos <https://tqtezos.com/>
 #
 # SPDX-License-Identifier: MPL-2.0
-{ pkgs ? import <nixpkgs> { }, timestamp ? "19700101" }:
+
+{ writeTextDir, runCommand, buildEnv, timestamp, bundled, source }:
 let
-  ignored_closures = [
-    "deb-packages"
-    "rpm-packages"
-    "deb-source-packages"
-    "rpm-source-packages"
-    "test-binaries"
-    "binaries-drv"
-  ];
-  closures = builtins.attrValues
-    (pkgs.lib.filterAttrs (n: v: !(builtins.elem n ignored_closures))
-      (import ./. { inherit pkgs timestamp; }));
-in pkgs.runCommandNoCC "release" { inherit closures; } ''
-  mkdir -p $out
-  for closure in $closures; do
-  cp $closure/* $out/
-  done
-''
+  protocols = import ./protocols.nix;
+  release-binaries = import ./release-binaries.nix;
+  release-notes = writeTextDir "release-notes.md" ''
+    Automatic release on ${builtins.substring 0 8 timestamp}
+
+    This release contains assets based on [revision ${source.rev}](https://gitlab.com/tezos/tezos/tree/${source.rev}) of ${source.ref} branch.
+
+    Binaries without protocol suffixes support the following protocols (unsupported protocols are listed for reference):
+    ${builtins.concatStringsSep "\n" (map (x: "- [ ] `${x}`") protocols.ignored
+      ++ map (x: "- [x] `${x}`") (protocols.allowed ++ protocols.active))}
+
+    Descriptions for binaries included in this release:
+    ${builtins.concatStringsSep "\n"
+    (map ({ name, description, ... }: "- `${name}`: ${description}")
+      release-binaries)}
+  '';
+  releaseNoTarball = buildEnv {
+    name = "tezos-release-no-tarball";
+    paths = [ "${bundled.binaries}/bin" LICENSE release-notes ];
+  };
+  releaseTarball = runCommand "release-tarball" { }
+    "mkdir $out; tar --owner=serokell:1000 --mode='u+rwX' -czhf $out/release.tar.gz -C ${releaseNoTarball} .";
+  LICENSE = writeTextDir "LICENSE" (builtins.readFile "${source}/LICENSE");
+in buildEnv {
+  name = "tezos-release";
+  paths = [ releaseNoTarball releaseTarball ];
+}

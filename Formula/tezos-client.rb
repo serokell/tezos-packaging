80de65b7b0c3744dad1b22dc061dc7d7b0799839
Roman Melnikov <roman.melnikov@serokell.io>
2021-01-19 15:58:56 +0300

    [#103] Add script for bottles building
    Problem: We want to provide bottles for our packages, thus users won't
    have to build them from scratch. Building them completely manually is
    quite inconvenient.
    
    Solution: Add script for bottles generation, which build bottles for all
    packages. This script is supposed to be run on different macOS version
    to cover all of them with the set of bottles. It should be run before
    release, so that corresponding formulas can be updated with the sha256
    hashes. Built bottles should be uploaded to the github releases, so that
    it's possible to fetch them while installing formula via
    ```
    brew tap serokell/tezos-packaging https://github.com/serokell/tezos-packaging.git
    brew install tezos-client
    ```

--- Formula/tezos-client.rb
+++ Formula/tezos-client.rb
@@ -8,6 +8,12 @@ class TezosClient < Tezos
   init
   desc "CLI client for interacting with tezos blockchain"
 
+  bottle do
+    root_url "https://github.com/serokell/tezos-packaging/releases/download/auto-release"
+    cellar :any
+    sha256 "bd2914e75b77de28b1362832023deda078025a68a046011e26c5efb05e9a4497" => :mojave
+  end
+
   def install
     make_deps
     install_template "src/bin_client/main_client.exe",

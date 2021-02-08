49112940d30e860fdd995bcd0076d5fca8f94668
Roman Melnikov <roman.melnikov@serokell.io>
2021-02-08 16:44:27 +0300

    [#134] Move abstract formula to a different dir
    Problem: `brew` tap checks all *.rb files in Formula directory for
    being a valid brew Formula. If one of the is invalid, the whole tap is
    broken:(
    `Formula/tezos.rb` is an abstract class for all other *.rb files and
    thus an invalid brew Formula.
    
    Solution: Move tezos.rb do a dedicated directory, thus it won't be
    checked during `brew tap`.

--- Formula/tezos-baker-008-PtEdoTez.rb
+++ Formula/tezos-baker-008-PtEdoTez.rb
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: LicenseRef-MIT-TQ
 
-require File.join(File.dirname(__FILE__), "tezos")
+require File.join(File.dirname(__FILE__), "..", "FormulaAbstract", "tezos")
 
 class TezosBaker008Ptedotez < Tezos
   init

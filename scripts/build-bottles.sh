d55f1a5bedef047867075ffc12861dac5ba19ed3
Roman Melnikov <roman.melnikov@serokell.io>
2021-03-03 17:29:25 +0300

    [#130] Update bottling script
    Problem: Now our formulas depend on each other, thus we have to build
    dependencies (specifically, tezos-sapling-params) earlier and shouldn't
    delete them. Also, now there are meta-formulas that shouldn't be
    bottled.
    
    Solution: Rework bottling script, list formulas that needs to be bottled
    manually (otherwise, we'll need some complex checking in the bash code
    which is quite error-prone). Also, handle tezos-sapling-params formula
    separately, because it's used as a dependency for tezos-client,
    tezos-node and tezos-baker.

--- scripts/build-bottles.sh
+++ scripts/build-bottles.sh
@@ -3,13 +3,29 @@
 #
 # SPDX-License-Identifier: LicenseRef-MIT-TQ
 
-for formula in ./Formula/*.rb; do
-    if [[ $formula != "*/tezos.rb" ]]; then
-        brew install --build-bottle "$formula"
-        brew bottle --force-core-tap "$formula"
-        brew uninstall "$formula"
-    fi
-done
+build_bottle () {
+    brew install --build-bottle "$1"
+    brew bottle --force-core-tap "$1"
+    brew uninstall "$1"
+}
+
+# tezos-sapling-params is used as a dependency for some of the formulas
+# so we handle it separately
+brew install --build-bottle ./Formula/tezos-sapling-params.rb
+brew bottle --force-core-tap ./Formula/tezos-sapling-params.rb
+
+# we don't bottle meta-formulas that contains only services
+build_bottle ./Formula/tezos-accuser-008-PtEdo2Zk.rb
+build_bottle ./Formula/tezos-admin-client.rb
+build_bottle ./Formula/tezos-baker-008-PtEdo2Zk.rb
+build_bottle ./Formula/tezos-client.rb
+build_bottle ./Formula/tezos-codec.rb
+build_bottle ./Formula/tezos-endorser-008-PtEdo2Zk.rb
+build_bottle ./Formula/tezos-node.rb
+build_bottle ./Formula/tezos-sandbox.rb
+build_bottle ./Formula/tezos-signer.rb
+
+brew uninstall ./Formula/tezos-sapling-params.rb
 # https://github.com/Homebrew/brew/pull/4612#commitcomment-29995084
 for bottle in ./*.bottle.*; do
     mv "$bottle" "${bottle/--/-}"

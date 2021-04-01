ac0712813e577eefe961294308264ff359e30533
pasqu4le <pasqu4le@gmail.com>
2021-04-01 13:15:51 +0200

    Skip rebuild check in formulae bottling
    Problem: Newer brew versions fail when checking for a rebuild version
    of non-core taps when building bottles.
    
    Solution: skip the check with '--no-rebuild'.
    Additionally, add the '--formula' option to brew 'install' to avoid
    the cask warning message.

--- scripts/build-bottles.sh
+++ scripts/build-bottles.sh
@@ -3,16 +3,18 @@
 #
 # SPDX-License-Identifier: LicenseRef-MIT-TQ
 
+# Newer brew versions fail when checking for a rebuild version of non-core taps.
+# So for now we skip the check with '--no-rebuild'
 build_bottle () {
-    brew install --build-bottle "$1"
-    brew bottle --force-core-tap "$1"
+    brew install --formula --build-bottle "$1"
+    brew bottle --force-core-tap --no-rebuild "$1"
     brew uninstall "$1"
 }
 
 # tezos-sapling-params is used as a dependency for some of the formulas
 # so we handle it separately
-brew install --build-bottle ./Formula/tezos-sapling-params.rb
-brew bottle --force-core-tap ./Formula/tezos-sapling-params.rb
+brew install --formula --build-bottle ./Formula/tezos-sapling-params.rb
+brew bottle --force-core-tap --no-rebuild ./Formula/tezos-sapling-params.rb
 
 # we don't bottle meta-formulas that contains only services
 build_bottle ./Formula/tezos-accuser-008-PtEdo2Zk.rb

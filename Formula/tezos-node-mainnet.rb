33839ee39758cfc241e7a953eb0c04563a92f9a9
pasqu4le <pasqu4le@gmail.com>
2021-04-16 12:13:29 +0200

    Fix typo in node formula description

--- Formula/tezos-node-mainnet.rb
+++ Formula/tezos-node-mainnet.rb
@@ -9,7 +9,7 @@ class TezosNodeMainnet < Formula
   bottle :unneeded
   depends_on "tezos-node"
 
-  desc "Meta formula that provides backround tezos-node service that runs on mainnet"
+  desc "Meta formula that provides background tezos-node service that runs on mainnet"
 
   def install
     startup_contents =

ddcceb42df000b80fbb5d99f8015a53531d2a863
Roman Melnikov <roman.melnikov@serokell.io>
2021-05-10 16:26:36 +0300

    [#195] Dynamically generate pipeline via python
    Problem: Some of the ci steps take huge amount of time. We run them on
    any changes even when the changes don't affect any of it.
    
    Solution: Make it possible to provide regexes, which will define paths,
    whose changes will trigger step to run. Pipeline from the yaml file
    is loaded to the python dictionary, this dict is later filtered
    basing on the output of 'git diff origin/master...HEAD --name-only'
    or 'git diff --name-only HEAD HEAD~1' in case we're in master branch.
    The files from diff list are matched agains the regexes from the
    pipeline config. If step doesn't match any of them, then it isn't
    included in the resulting generated pipeline config.
    
    To generate pipeline config locally run:
    ```
    ./.buildkite/generate-pipeline.sh
    ```

---
+++ .buildkite/generate-pipeline.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env nix-shell
+#! nix-shell shell.nix -i bash
+
+# SPDX-FileCopyrightText: 2021 TQ Tezos <https://tqtezos.com/>
+#
+# SPDX-License-Identifier: LicenseRef-MIT-TQ
+
+set -euo pipefail
+
+git fetch origin master
+
+diff="$(git diff origin/master...HEAD --name-only)"
+
+# Diff with the master branch is empty when we're in the master branch,
+# so we're using 'git diff --name-only HEAD HEAD~1' instead
+if [[ -z $diff ]]; then
+  diff="$(git diff --name-only HEAD~1 HEAD)"
+fi
+
+.buildkite/filter-pipeline.py --pipeline .buildkite/pipeline-raw.yml \
+  --git-diff "$diff" --output .buildkite/pipeline.yml

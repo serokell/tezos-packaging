32aac868663941aa4a6bab77eab9ee31895b9e8e
Ilona Prikule <ilona.prikule@serokell.io>
2021-08-31 06:57:01 +0200

    [#284] Add script automating bottle hash insertion
    Problem: every time after a new release, we build brew bottles,
    and their SHA256 hashes should be added to the brew formulae.
    Right now we do this manually, but it's a tedious and error-prone
    process that would be nice to automate.
    
    Solution: added a bash script that can do this. It takes a directory
    where the bottle.tar.gz files are placed as its argument and inserts
    the computed hash for the respective OS into the right formula.
    It is supposed to be run from the base directory (tezos-packaging).

---
+++ scripts/bottle-hashes.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+# SPDX-FileCopyrightText: 2021 TQ Tezos <https://tqtezos.com/>
+#
+# SPDX-License-Identifier: LicenseRef-MIT-TQ
+
+# This script takes a directory where the bottles are stored as its argument.
+# Run it from the base directory (tezos-packaging).
+
+if [[ -d ./Formula ]]
+then
+    if [[ -d "$1" ]]
+    then
+        regex="(tezos-.*)-v.*\.(catalina|mojave)\.bottle\.tar\.gz"
+        for bottle in "$1"/tezos-*.bottle.tar.gz; do
+            if [[ $bottle =~ $regex ]]; then
+                bottle_hash=`sha256sum "$bottle" | cut -d " " -f 1`
+                formula_name="${BASH_REMATCH[1]}"
+                os="${BASH_REMATCH[2]}"
+
+                if [[ -f "./Formula/$formula_name.rb" ]]; then
+                    line="\    sha256 cellar: :any, $os: \"$bottle_hash\""
+                    sed -i "/root_url.*/a $line" ./Formula/$formula_name.rb
+                fi
+            fi
+        done
+    else
+        echo "The passed directory $1 does not exist."
+    fi
+else
+    echo "Please run this script from the base directory (tezos-packaging)."
+fi

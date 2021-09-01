c1e04dc4fa7f971b3b13eb554e79eac2c65bc6c8
Zhenya Vinogradov <zhenyavinogradov@gmail.com>
2021-09-01 10:13:57 +0300

    [#285] Test that CI attaches packages to the release

--- scripts/build-bottles.sh
+++ scripts/build-bottles.sh
@@ -6,15 +6,20 @@
 # Newer brew versions fail when checking for a rebuild version of non-core taps.
 # So for now we skip the check with '--no-rebuild'
 build_bottle () {
-    brew install --formula --build-bottle "$1"
-    brew bottle --force-core-tap --no-rebuild "$1"
-    brew uninstall "$1"
+    # brew install --formula --build-bottle "$1"
+    # brew bottle --force-core-tap --no-rebuild "$1"
+    # brew uninstall "$1"
+    FORMULA_FILE_NAME=$(basename $1)
+    PACKAGE_NAME=${FORMULA_FILE_NAME%.rb}
+    BOTTLE_FILE_NAME=${PACKAGE_NAME}--$VER_PKG.$VER_MACOS.bottle.tar.gz
+    echo bla > package
+    tar --create --verbose -z -f ./$BOTTLE_FILE_NAME package
 }
 
 # tezos-sapling-params is used as a dependency for some of the formulas
 # so we handle it separately.
 # We don't build the bottle for it because it is never updated over time.
-brew install --formula ./Formula/tezos-sapling-params.rb
+# brew install --formula ./Formula/tezos-sapling-params.rb
 
 # we don't bottle meta-formulas that contains only services
 build_bottle ./Formula/tezos-accuser-010-PtGRANAD.rb
@@ -27,7 +32,7 @@ build_bottle ./Formula/tezos-node.rb
 build_bottle ./Formula/tezos-sandbox.rb
 build_bottle ./Formula/tezos-signer.rb
 
-brew uninstall ./Formula/tezos-sapling-params.rb
+# brew uninstall ./Formula/tezos-sapling-params.rb
 # https://github.com/Homebrew/brew/pull/4612#commitcomment-29995084
 for bottle in ./*.bottle.*; do
     mv "$bottle" "${bottle/--/-}"

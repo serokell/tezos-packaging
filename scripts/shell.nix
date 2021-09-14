82fbab12a89a27f42c8dcf9c6569777bbec5c805
Zhenya Vinogradov <zhenyavinogradov@gmail.com>
2021-09-14 17:01:04 +0300

    Remove nix overlay for `gh` tool
    Nixpkgs has a newer version now

--- scripts/shell.nix
+++ scripts/shell.nix
@@ -2,27 +2,7 @@
 #
 # SPDX-License-Identifier: LicenseRef-MIT-TQ
 
-let
-  overlays =
-    [
-      (self: super: {
-        gh = (super.callPackage "${super.path}/pkgs/applications/version-management/git-and-tools/gh" {
-          buildGoModule = args: super.buildGoModule (args // rec {
-            version = "1.2.0";
-            vendorSha256 = "0ybbwbw4vdsxdq4w75s1i0dqad844sfgs69b3vlscwfm6g3i9h51";
-            src = self.fetchFromGitHub {
-              owner = "cli";
-              repo = "cli";
-              rev = "v${version}";
-              sha256 = "17hbgi1jh4p07r4p5mr7w7p01i6zzr28mn5i4jaki7p0jwfqbvvi";
-            };
-          });
-        });
-      })
-    ];
-in
-
-{ pkgs ? import (import ../nix/nix/sources.nix {}).nixpkgs { inherit overlays; } }:
+{ pkgs ? import (import ../nix/nix/sources.nix {}).nixpkgs { } }:
 with pkgs;
 mkShell {
   buildInputs = [ gh git rename gnupg ];

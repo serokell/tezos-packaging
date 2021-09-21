d9d4f6f0f776db234cb2ae6879e9de05692bf859
Roman Melnikov <roman.melnikov@serokell.io>
2021-09-21 18:13:37 +0300

    [#287] Add script for native packages publishing
    Problem: We want to automate our release routine as much as possible.
    Native packages publishing is one of the parts of this routine.
    
    Solution: Add script that publishes Fedora and Ubuntu packages from the
    given directory.

---
+++ scripts/publish-native-packages.sh
@@ -0,0 +1,59 @@
+#! /usr/bin/env nix-shell
+#! nix-shell shell.nix -i bash
+# SPDX-FileCopyrightText: 2021 TQ Tezos <https://tqtezos.com/>
+#
+# SPDX-License-Identifier: LicenseRef-MIT-TQ
+
+set -euo pipefail
+
+source_packages_path="$1"
+
+# Basic dput configuration
+cat >dput.cfg <<EOL
+[DEFAULT]
+login	 = *
+method = ftp
+hash = md5
+allow_unsigned_uploads = 0
+allow_dcut = 0
+run_lintian = 0
+run_dinstall = 0
+check_version = 0
+scp_compress = 0
+post_upload_command	=
+pre_upload_command =
+passive_ftp = 1
+default_host_main	=
+allowed_distributions	= (?!UNRELEASED)
+
+[tezos-serokell]
+fqdn      = ppa.launchpad.net
+method    = ftp
+incoming  = ~serokell/ubuntu/tezos
+login     = anonymous
+
+[tezos-rc-serokell]
+fqdn        = ppa.launchpad.net
+method      = ftp
+incoming    = ~serokell/ubuntu/tezos-rc
+login       = anonymous
+EOL
+
+if [[ $TEZOS_VERSION =~ v.*-rc[0-9]* ]]; then
+  launchpad_ppa="tezos-rc-serokell"
+  copr_project="@Serokell/Tezos-rc"
+else
+  launchpad_ppa="tezos-serokell"
+  copr_project="@Serokell/Tezos"
+fi
+
+for f in "$source_packages_path"/*.changes; do
+  sed -i 's/^Changed-By: .*$/Changed-By: Serokell <tezos-packaging@serokell.io>/' "$f"
+  debsign "$f"
+  execute-dput -c dput.cfg "$launchpad_ppa" "$f"
+done
+
+for f in "$source_packages_path"/*.src.rpm; do
+  rpmsign --define="%_gpg_name Serokell <tezos-packaging@serokell.io>" --define="%__gpg $(which gpg)" --addsign "$f"
+  /run/wrappers/bin/sudo -u copr-uploader /run/current-system/sw/bin/copr-cli build "$copr_project" --nowait "$f"
+done

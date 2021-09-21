b202d450ada3ba38e0b068b3dedb24dfefb7cfc8
Ilona Prikule <ilona.prikule@serokell.io>
2021-09-22 01:58:14 +0200

    [#286] Add script to bump brew versions
    Problem: before adding new bottle hashes, we need to update versions
    and tags in formulae and clean out old hashes. Currently there is no
    way to easily do it automatically.
    
    Solution: added a bash script that does exactly that.

---
+++ scripts/update-brew-formulae.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+# SPDX-FileCopyrightText: 2021 TQ Tezos <https://tqtezos.com/>
+#
+# SPDX-License-Identifier: LicenseRef-MIT-TQ
+
+# This script takes the new tag as its argument.
+# Run it from the base directory (tezos-packaging).
+
+if [[ -d ./Formula ]]
+then
+    regex="(v.*)-[0-9]*"
+    if [[ $1 =~ $regex ]]; then
+        tag="${BASH_REMATCH[0]}"
+        version="${BASH_REMATCH[1]}"
+        find ./Formula -type f \( -name 'tezos-*.rb' ! -name 'tezos-sapling-params.rb' \) \
+            -exec sed -i "s/version \"v.*\"/version \"$tag\"/g" {} \; \
+            -exec sed -i "s/:tag => \"v.*\"/:tag => \"$version\"/g" {} \; \
+            -exec sed -i "/catalina/d" {} \; \
+            -exec sed -i "/mojave/d" {} \;
+    else
+        echo "The argument does not look like a tag, which should have a form of 'v*-[0-9]*'"
+    fi
+else
+    echo "Please run this script from the base directory (tezos-packaging)."
+fi

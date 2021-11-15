0cd8b7d204c26ae5f2043992662e08ef91701a36
Roman Melnikov <roman.melnikov@serokell.io>
2021-11-15 17:12:50 +0300

    fixup! [Chore] Sign formulae update in a separate step

--- scripts/sign-commits.sh
+++ scripts/sign-commits.sh
@@ -9,19 +9,19 @@ set -e
 
 git config user.name "serokell-bot" # necessary for pushing
 git config user.email "tezos-packaging@serokell.io"
-git fetch --all
-# We call this script only after a commit was pushed to the given branch, so it's safe to switch to it
-git switch "$BUILDKITE_BRANCH"
-git reset --hard origin/"$BUILDKITE_BRANCH"
 
-# Find if there are unsigned commits in the current branch
-# If there are grep matches then there are unsigned commits
-if git log --pretty="format:%G?" "origin/$BUILDKITE_PIPELINE_DEFAULT_BRANCH..$BUILDKITE_BRANCH" --first-parent | grep "N$"; then
-  echo "Found unsigned commits"
-  # Try to sign and push signed commits, retry in case of collision
-  while : ; do
-    git fetch --all
-    git reset --hard origin/"$BUILDKITE_BRANCH"
+# Try to sign and push signed commits, retry in case of collision
+while : ; do
+  git fetch --all
+  # We call this script only after a commit was pushed to the given branch, so it's safe to switch to it
+  git switch "$BUILDKITE_BRANCH"
+  git reset --hard origin/"$BUILDKITE_BRANCH"
+  # Find if there are unsigned commits in the current branch
+  # If there are grep matches then there are unsigned commits
+  if git log --pretty="format:%G?" "origin/$BUILDKITE_PIPELINE_DEFAULT_BRANCH..$BUILDKITE_BRANCH" | grep "N$"; then
+    echo "Found unsigned commits"
+    # Rebase through all commits in the current branch and sign them, if some of the commits are already
+    # signed, a signature will be overridden
     if ! git rebase --exec 'git commit --amend -n --gpg-sign="tezos-packaging@serokell.io" --no-edit' \
       "origin/$BUILDKITE_PIPELINE_DEFAULT_BRANCH"; then
       git rebase --abort
@@ -29,7 +29,8 @@ if git log --pretty="format:%G?" "origin/$BUILDKITE_PIPELINE_DEFAULT_BRANCH..$BU
     fi
     # This should fail in case we're trying to overwrite some new commits
     ! git push --force-with-lease || break
-  done
-else
-  echo "No unsigned commits found"
-fi
+  else
+    echo "No unsigned commits found"
+    exit 0
+  fi
+done

b8ace159a3a2d32e4dbed92a3d2fdcfda8d5449d
Roman Melnikov <roman.melnikov@serokell.io>
2021-12-09 17:52:37 +0300

    [Chore] Bump tezos-baking letter version
    Problem: We're going to release update for tezos-baking that will remove
    granada daemons from mainnet baking service.
    
    Solution: Bump tezos-baking letter version in order to be able to
    release update.

--- docker/package/model.py
+++ docker/package/model.py
@@ -473,7 +473,7 @@ class TezosBakingServicesPackage(AbstractPackage):
     # native releases, so we append an extra letter to the version of
     # the package.
     # This should be reset to "" whenever the native version is bumped.
-    letter_version = ""
+    letter_version = "a"
 
     def __init__(
         self,

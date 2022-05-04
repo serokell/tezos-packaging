5e604124207eb3c79e19d8792af921bd4be6ba1b
Ilona Prikule <ilona.prikule@serokell.io>
2022-05-04 02:39:51 +0200

    [#418] Reduce bottle building in BK to one step
    Problem: currently, after each release, we partially generate the
    Buildkite pipeline to handle building brew bottles for formulae
    relevant to the release. This script has been extended since its
    conception and is becoming difficult to maintain.
    
    Solution: reworked the BK tags pipeline to include a single step
    for each OS, calling an external script that filters the formulae
    to the list of those that have been changed, and builds and uploads
    them one-by-one.

---
+++ scripts/build-all-bottles.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env nix-shell
+#! nix-shell shell.nix -i bash
+
+# SPDX-FileCopyrightText: 2022 Oxhead Alpha
+# SPDX-License-Identifier: LicenseRef-MIT-OA
+
+if [ -z "$1" ] ; then
+    echo "Please call this script with the name of the OS for which to build the bottles."
+    exit 1
+fi
+
+set -euo pipefail
+
+# we don't bottle meta-formulas that contain only services
+formulae=("tezos-accuser-011-PtHangz2" "tezos-accuser-012-Psithaca" "tezos-admin-client" "tezos-baker-011-PtHangz2" "tezos-baker-012-Psithaca" "tezos-client" "tezos-codec" "tezos-endorser-011-PtHangz2" "tezos-node" "tezos-sandbox" "tezos-signer")
+
+# tezos-sapling-params is used as a dependency for some of the formulas
+# so we handle it separately.
+# We don't build the bottle for it because it is never updated over time.
+brew install --formula ./Formula/tezos-sapling-params.rb
+
+for f in "${formulae[@]}"; do
+  # check if the formula doesn't already have a bottle in its respective release
+  if ./scripts/check-bottle-built.sh "$f" "$1"; then
+    # build a bottle
+    if ./scripts/build-one-bottle.sh "$f"; then
+      # upload the bottle to its respective release
+      FORMULA_TAG="$(sed -n "s/^\s\+version \"\(.*\)\"/\1/p" "./Formula/$f.rb")"
+      gh release upload "$FORMULA_TAG" "$f*.bottle.*" ||
+        >&2 echo "Bottle for $f couldn't be uploaded to $FORMULA_TAG release."
+    else
+      >&2 echo "Bottle for $f couldn't be built."
+    fi
+  fi
+done
+
+brew uninstall ./Formula/tezos-sapling-params.rb
+# opam doesn't always handle the situation when the same library is present for
+# multiple architectures, see https://github.com/ocaml/opam-repository/issues/20941
+# so all dependencies are cleared after bottles builds to avoid errors
+brew autoremove

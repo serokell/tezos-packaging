67e8ada628b8e02600042ec7949adbc08434a3bc
Roman Melnikov <roman.melnikov@serokell.io>
2022-05-18 10:47:12 +0300

    [Chore] Hotfix 'build-binary.sh' script
    Problem: After recently applied shellcheck suggestions,
    'build-binary.sh' doesn't provide a list of opam files to install
    correctly.
    
    Solution: Store find result in the array instead of a single string
    variable to provide proper quoted expansion.

--- docker/package/scripts/build-binary.sh
+++ docker/package/scripts/build-binary.sh
@@ -15,8 +15,11 @@ cd tezos
 opam init local ../opam-repository --bare --disable-sandboxing
 opam switch create . --repositories=local
 eval "$(opam env)"
-opams="$(find ./vendors ./src ./tezt -name \*.opam -print)"
-opam install "$opams" --deps-only --criteria="-notuptodate,-changed,-removed"
+opams=()
+while IFS=  read -r -d $'\0'; do
+    opams+=("$REPLY")
+done < <(find ./vendors ./src ./tezt -name \*.opam -print0)
+opam install "${opams[@]}" --deps-only --criteria="-notuptodate,-changed,-removed"
 eval "$(opam env)"
 dune build "$dune_filepath"
 cp "./_build/default/$dune_filepath" "../$binary_name"

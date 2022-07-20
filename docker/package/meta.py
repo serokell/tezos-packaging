4f8d648da838f63a3334eb302c278d526ec433eb
z8v <86471569+z8v@users.noreply.github.com>
2022-07-20 23:30:59 +0300

    Add ability to override the license version
    Problem: We cannot use a version that is not a tag because we're
    fetching the LICENSE for that specific tag.
    
    Solution: Add the option to override the license version and use the
    default if not given.

--- docker/package/meta.py
+++ docker/package/meta.py
@@ -6,21 +6,27 @@ import os, json
 
 
 class PackagesMeta:
-    def __init__(self, version, release, ubuntu_epoch, fedora_epoch, maintainer):
+    def __init__(
+        self, version, release, ubuntu_epoch, fedora_epoch, maintainer, license_version
+    ):
         self.version = version
         self.release = release
         self.ubuntu_epoch = ubuntu_epoch
         self.fedora_epoch = fedora_epoch
         self.maintainer = maintainer
+        self.license_version = license_version
 
+
+version = os.environ["TEZOS_VERSION"][1:]
 
 meta_json_contents = json.load(
     open(f"{os.path.dirname(__file__)}/../../meta.json", "r")
 )
 packages_meta = PackagesMeta(
-    version=os.environ["TEZOS_VERSION"][1:],
+    version=version,
     release=str(meta_json_contents["release"]),
     ubuntu_epoch=2,
     fedora_epoch=1,
     maintainer=meta_json_contents["maintainer"],
+    license_version=os.getenv("TEZOS_LICENSE_VERSION", f"v{version}"),
 )

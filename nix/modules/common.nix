serokell/tezos-packaging-rc

[Chore] Update nix building
Problem: Since `v15.0-rc1`, binaries were
renamed. Thus, building process should be updated.

Solution: Update packages names.

nix/modules/common.nix
@@ -21,11 +21,11 @@ rec {
   daemonOptions = sharedOptions // {
 
     baseProtocols = mkOption {
-      type = types.listOf (types.enum [ "013-PtJakart" "014-PtKathma" ]);
+      type = types.listOf (types.enum [ "PtKathma" "PtLimaPt" ]);
       description = ''
         List of protocols for which daemons will be run.
       '';
-      example = ["013-PtJakart"];
+      example = ["PtKathma"];
     };
 
     rpcPort = mkOption {
@@ -33,7 +33,7 @@ rec {
       default = 8732;
       example = 8732;
       description = ''
-        Tezos node RPC port.
+        Octez node RPC port.
       '';
     };
 
@@ -50,17 +50,17 @@ rec {
     mkIf (instancesCfg != {}) {
       users = mkMerge (flip mapAttrsToList instancesCfg (node-name: node-cfg: genUsers node-name ));
       systemd = mkMerge (flip mapAttrsToList instancesCfg (node-name: node-cfg:
-        let tezos-client = "${pkgs.tezosPackages.tezos-client}/bin/tezos-client";
+        let octez-client = "${pkgs.octezPackages.octez-client}/bin/octez-client";
             passwordFilenameArg = if node-cfg.passwordFilename != null then "-f ${node-cfg.passwordFilename}" else "";
         in {
-          services."tezos-${node-name}-tezos-${service-name}" = lib.recursiveUpdate (genSystemdService node-name node-cfg service-name) rec {
-            bindsTo = [ "network.target" "tezos-${node-name}-tezos-node.service" ];
+          services."octez-${node-name}-octez-${service-name}" = lib.recursiveUpdate (genSystemdService node-name node-cfg service-name) rec {
+            bindsTo = [ "network.target" "octez-${node-name}-octez-node.service" ];
             after = bindsTo;
             path = with pkgs; [ curl ];
             preStart =
               ''
                 while ! _="$(curl --silent http://localhost:${toString node-cfg.rpcPort}/chains/main/blocks/head/)"; do
-                  echo "Trying to connect to tezos-node"
+                  echo "Trying to connect to octez-node"
                   sleep 1s
                 done
 
@@ -69,10 +69,10 @@ rec {
 
                 # Generate or update service config file
                 if [[ ! -f "$service_data_dir/config" ]]; then
-                  ${tezos-client} -d "$service_data_dir" -E "http://localhost:${toString node-cfg.rpcPort}" ${passwordFilenameArg} \
+                  ${octez-client} -d "$service_data_dir" -E "http://localhost:${toString node-cfg.rpcPort}" ${passwordFilenameArg} \
                   config init --output "$service_data_dir/config" >/dev/null 2>&1
                 else
-                  ${tezos-client} -d "$service_data_dir" -E "http://localhost:${toString node-cfg.rpcPort}" ${passwordFilenameArg} \
+                  ${octez-client} -d "$service_data_dir" -E "http://localhost:${toString node-cfg.rpcPort}" ${passwordFilenameArg} \
                   config update >/dev/null 2>&1
                 fi
               '' + service-prestart-script node-cfg;
@@ -85,20 +85,20 @@ rec {
     };
 
   genUsers = node-name: {
-    groups."tezos-${node-name}" = { };
-    users."tezos-${node-name}" = { group = "tezos-${node-name}"; isNormalUser = true; };
+    groups."octez-${node-name}" = { };
+    users."octez-${node-name}" = { group = "octez-${node-name}"; isNormalUser = true; };
   };
 
   genSystemdService = node-name: node-cfg: service-name: {
     wantedBy = [ "multi-user.target" ];
-    description = "Tezos ${service-name}";
+    description = "Octez ${service-name}";
     environment = {
-      TEZOS_LOG = "* -> ${node-cfg.logVerbosity}";
+      OCTEZ_LOG = "* -> ${node-cfg.logVerbosity}";
     };
     serviceConfig = {
-      User = "tezos-${node-name}";
-      Group = "tezos-${node-name}";
-      StateDirectory = "tezos-${node-name}";
+      User = "octez-${node-name}";
+      Group = "octez-${node-name}";
+      StateDirectory = "octez-${node-name}";
       Restart = "always";
       RestartSec = "10";
     };

nix/modules/tezos-node.nix
@@ -6,8 +6,8 @@
 with lib;
 
 let
-  tezos-node-pkg = pkgs.tezosPackages.tezos-node;
-  cfg = config.services.tezos-node;
+  octez-node-pkg = pkgs.octezPackages.octez-node;
+  cfg = config.services.octez-node;
   genConfigCommand = historyMode: rpcPort: netPort: network: options: ''
     --data-dir "$node_data_dir" \
     --history-mode "${historyMode}" \
@@ -18,10 +18,10 @@ let
   common = import ./common.nix { inherit lib; inherit pkgs; };
   instanceOptions = types.submodule ( {...} : {
     options = common.sharedOptions // {
-      enable = mkEnableOption "Tezos node service";
+      enable = mkEnableOption "Octez node service";
 
       package = mkOption {
-        default = tezos-node-pkg;
+        default = octez-node-pkg;
         type = types.package;
       };
 
@@ -30,7 +30,7 @@ let
         default = 8732;
         example = 8732;
         description = ''
-          Tezos node RPC port.
+          Octez node RPC port.
         '';
       };
 
@@ -39,13 +39,13 @@ let
         default = 9732;
         example = 9732;
         description = ''
-          Tezos node net port.
+          Octez node net port.
         '';
       };
 
       network = mkOption {
         type = types.str;
-        default = "jakartanet";
+        default = "kathmandunet";
         description = ''
           Network which node will be running on.
           Can be either a predefined network name or a URL to the network config.
@@ -65,7 +65,7 @@ let
         type = types.listOf types.str;
         default = [];
         description = ''
-          Additional 'tezos-node' options that affect configuration file.
+          Additional 'octez-node' options that affect configuration file.
         '';
       };
 
@@ -75,14 +75,14 @@ let
         description = ''
           Custom node config.
           This option overrides the all other options that affect
-          tezos-node config.
+          octez-node config.
         '';
       };
     };
   });
 in {
 
-  options.services.tezos-node = {
+  options.services.octez-node = {
     instances = mkOption {
       type = types.attrsOf instanceOptions;
       description = "Configuration options";
@@ -92,7 +92,7 @@ in {
   config = mkIf (cfg.instances != {}) {
     users = mkMerge (flip mapAttrsToList cfg.instances (node-name: node-cfg: common.genUsers node-name ));
     systemd = mkMerge (flip mapAttrsToList cfg.instances (node-name: node-cfg: {
-      services."tezos-${node-name}-tezos-node" = common.genSystemdService node-name node-cfg "node" // {
+      services."octez-${node-name}-octez-node" = common.genSystemdService node-name node-cfg "node" // {
         after = [ "network.target" ];
         preStart =
           ''
@@ -104,10 +104,10 @@ in {
               ''
                 # Generate or update node config file
                 if [[ ! -f "$node_data_dir/config.json" ]]; then
-                  ${node-cfg.package}/bin/tezos-node config init \
+                  ${node-cfg.package}/bin/octez-node config init \
                   ${genConfigCommand node-cfg.historyMode node-cfg.rpcPort node-cfg.netPort node-cfg.network node-cfg.additionalOptions}
                 else
-                  ${node-cfg.package}/bin/tezos-node config update \
+                  ${node-cfg.package}/bin/octez-node config update \
                   ${genConfigCommand node-cfg.historyMode node-cfg.rpcPort node-cfg.netPort node-cfg.network node-cfg.additionalOptions}
                 fi
               ''
@@ -117,7 +117,7 @@ in {
               ''
           );
         script = ''
-          ${node-cfg.package}/bin/tezos-node run --data-dir "$STATE_DIRECTORY/node/data"
+          ${node-cfg.package}/bin/octez-node run --data-dir "$STATE_DIRECTORY/node/data"
         '';
       };
     }));

nix/modules/tezos-signer.nix
@@ -7,13 +7,13 @@
 with lib;
 
 let
-  tezos-signer-launch = "${pkgs.tezosPackages.tezos-signer}/bin/tezos-signer launch";
+  octez-signer-launch = "${pkgs.octezPackages.octez-signer}/bin/octez-signer launch";
   common = import ./common.nix { inherit lib; inherit pkgs; };
-  cfg = config.services.tezos-signer;
+  cfg = config.services.octez-signer;
   instanceOptions = types.submodule ( {...} : {
     options = common.sharedOptions // {
 
-      enable = mkEnableOption "Tezos signer service";
+      enable = mkEnableOption "Octez signer service";
 
       networkProtocol = mkOption {
         type = types.enum [ "http" "https" "tcp" "unix" ];
@@ -28,7 +28,7 @@ let
         default = "127.0.0.1";
         example = "127.0.0.1";
         description = ''
-          Tezos signer net address.
+          Octez signer net address.
         '';
       };
 
@@ -37,7 +37,7 @@ let
         default = 8080;
         example = 8080;
         description = ''
-          Tezos signer net port.
+          Octez signer net port.
         '';
       };
 
@@ -45,7 +45,7 @@ let
         type = types.str;
         default = null;
         description = ''
-          Path of the SSL certificate to use for https Tezos signer.
+          Path of the SSL certificate to use for https Octez signer.
         '';
       };
 
@@ -53,7 +53,7 @@ let
         type = types.str;
         default = null;
         description = ''
-          Key path to use for https Tezos signer.
+          Key path to use for https Octez signer.
         '';
       };
 
@@ -61,7 +61,7 @@ let
         type = types.str;
         default = null;
         description = ''
-          Socket to use for Tezos signer running over UNIX socket.
+          Socket to use for Octez signer running over UNIX socket.
         '';
       };
 
@@ -70,14 +70,14 @@ let
         default = 1;
         example = 1;
         description = ''
-          Timeout for Tezos signer.
+          Timeout for Octez signer.
         '';
       };
 
     };
   });
 in {
-  options.services.tezos-signer = {
+  options.services.octez-signer = {
     instances = mkOption {
       type = types.attrsOf instanceOptions;
       description = "Configuration options";
@@ -87,21 +87,21 @@ in {
   config = mkIf (cfg.instances != {}) {
     users = mkMerge (flip mapAttrsToList cfg.instances (node-name: node-cfg: common.genUsers node-name ));
     systemd = mkMerge (flip mapAttrsToList cfg.instances (node-name: node-cfg:
-      let tezos-signers = {
+      let octez-signers = {
         "http" =
-          "${tezos-signer-launch} http signer --address ${node-cfg.netAddress} --port ${toString node-cfg.netPort}";
+          "${octez-signer-launch} http signer --address ${node-cfg.netAddress} --port ${toString node-cfg.netPort}";
         "https" =
-          "${tezos-signer-launch} https signer ${node-cfg.certPath} ${node-cfg.keyPath} --address ${node-cfg.netAddress} --port ${toString node-cfg.netPort}";
+          "${octez-signer-launch} https signer ${node-cfg.certPath} ${node-cfg.keyPath} --address ${node-cfg.netAddress} --port ${toString node-cfg.netPort}";
         "tcp" =
-          "${tezos-signer-launch} socket signer --address ${node-cfg.netAddress} --port ${toString node-cfg.netPort} --timeout ${toString node-cfg.timeout}";
+          "${octez-signer-launch} socket signer --address ${node-cfg.netAddress} --port ${toString node-cfg.netPort} --timeout ${toString node-cfg.timeout}";
         "unix" =
-          "${tezos-signer-launch} local signer --socket ${node-cfg.unixSocket}";
+          "${octez-signer-launch} local signer --socket ${node-cfg.unixSocket}";
       };
       in {
-      services."tezos-${node-name}-tezos-signer" = common.genSystemdService node-name node-cfg "signer" // {
+      services."octez-${node-name}-octez-signer" = common.genSystemdService node-name node-cfg "signer" // {
         after = [ "network.target" ];
         script = ''
-          ${tezos-signers.${node-cfg.networkProtocol}}
+          ${octez-signers.${node-cfg.networkProtocol}}
         '';
       };
     }));

tests/tezos-modules.nix
@@ -17,7 +17,7 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
                 ../nix/modules/tezos-baker.nix
               ];
 
-    services.tezos-node.instances.jakartanet = {
+    services.octez-node.instances.kathmandunet = {
       enable = true;
       additionalOptions = [
         "--bootstrap-threshold=1"
@@ -25,19 +25,19 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
       ];
     };
 
-    services.tezos-signer.instances.jakartanet = {
+    services.octez-signer.instances.kathmandunet = {
       enable = true;
       networkProtocol = "http";
     };
 
-    services.tezos-accuser.instances.jakartanet = {
+    services.octez-accuser.instances.kathmandunet = {
       enable = true;
-      baseProtocols = ["013-PtJakart" "014-PtKathma"];
+      baseProtocols = ["PtKathma"];
     };
 
-    services.tezos-baker.instances.jakartanet = {
+    services.octez-baker.instances.kathmandunet = {
       enable = true;
-      baseProtocols = ["013-PtJakart" "014-PtKathma"];
+      baseProtocols = ["PtKathma"];
       bakerAccountAlias = "baker";
       bakerSecretKey = "unencrypted:edsk3KaTNj1d8Xd3kMBrZkJrfkqsz4XwwiBXatuuVgTdPye2KpE98o";
     };
@@ -48,16 +48,16 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
     start_all()
 
     services = [
-        "tezos-node",
-        "tezos-signer",
-        "tezos-baker",
-        "tezos-accuser",
+        "octez-node",
+        "octez-signer",
+        "octez-baker",
+        "octez-accuser",
     ]
 
     for s in services:
-        machine.wait_for_unit(f"tezos-jakartanet-{s}.service")
+        machine.wait_for_unit(f"octez-kathmandunet-{s}.service")
 
-    with subtest("check tezos-node rpc response"):
+    with subtest("check octez-node rpc response"):
         machine.wait_for_open_port(8732)
         machine.wait_until_succeeds(
             "curl --silent http://localhost:8732/chains/main/blocks/head/header | grep level"
@@ -65,6 +65,6 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
 
     with subtest("service status sanity check"):
         for s in services:
-            machine.succeed(f"systemctl status tezos-jakartanet-{s}.service")
+            machine.succeed(f"systemctl status octez-kathmandunet-{s}.service")
   '';
 }) { inherit pkgs system; }

serokell/tezos-packaging

nix/modules/common.nix
@@ -21,11 +21,11 @@ rec {
   daemonOptions = sharedOptions // {
 
     baseProtocols = mkOption {
-      type = types.listOf (types.enum [ "013-PtJakart" "014-PtKathma" ]);
+      type = types.listOf (types.enum [ "PtKathma" "PtLimaPt" ]);
       description = ''
         List of protocols for which daemons will be run.
       '';
-      example = ["013-PtJakart"];
+      example = ["PtKathma"];
     };
 
     rpcPort = mkOption {
@@ -33,7 +33,7 @@ rec {
       default = 8732;
       example = 8732;
       description = ''
-        Tezos node RPC port.
+        Octez node RPC port.
       '';
     };
 
@@ -50,17 +50,17 @@ rec {
     mkIf (instancesCfg != {}) {
       users = mkMerge (flip mapAttrsToList instancesCfg (node-name: node-cfg: genUsers node-name ));
       systemd = mkMerge (flip mapAttrsToList instancesCfg (node-name: node-cfg:
-        let tezos-client = "${pkgs.tezosPackages.tezos-client}/bin/tezos-client";
+        let octez-client = "${pkgs.octezPackages.octez-client}/bin/octez-client";
             passwordFilenameArg = if node-cfg.passwordFilename != null then "-f ${node-cfg.passwordFilename}" else "";
         in {
-          services."tezos-${node-name}-tezos-${service-name}" = lib.recursiveUpdate (genSystemdService node-name node-cfg service-name) rec {
-            bindsTo = [ "network.target" "tezos-${node-name}-tezos-node.service" ];
+          services."tezos-${node-name}-octez-${service-name}" = lib.recursiveUpdate (genSystemdService node-name node-cfg service-name) rec {
+            bindsTo = [ "network.target" "tezos-${node-name}-octez-node.service" ];
             after = bindsTo;
             path = with pkgs; [ curl ];
             preStart =
               ''
                 while ! _="$(curl --silent http://localhost:${toString node-cfg.rpcPort}/chains/main/blocks/head/)"; do
-                  echo "Trying to connect to tezos-node"
+                  echo "Trying to connect to octez-node"
                   sleep 1s
                 done
 
@@ -69,10 +69,10 @@ rec {
 
                 # Generate or update service config file
                 if [[ ! -f "$service_data_dir/config" ]]; then
-                  ${tezos-client} -d "$service_data_dir" -E "http://localhost:${toString node-cfg.rpcPort}" ${passwordFilenameArg} \
+                  ${octez-client} -d "$service_data_dir" -E "http://localhost:${toString node-cfg.rpcPort}" ${passwordFilenameArg} \
                   config init --output "$service_data_dir/config" >/dev/null 2>&1
                 else
-                  ${tezos-client} -d "$service_data_dir" -E "http://localhost:${toString node-cfg.rpcPort}" ${passwordFilenameArg} \
+                  ${octez-client} -d "$service_data_dir" -E "http://localhost:${toString node-cfg.rpcPort}" ${passwordFilenameArg} \
                   config update >/dev/null 2>&1
                 fi
               '' + service-prestart-script node-cfg;
@@ -91,9 +91,9 @@ rec {
 
   genSystemdService = node-name: node-cfg: service-name: {
     wantedBy = [ "multi-user.target" ];
-    description = "Tezos ${service-name}";
+    description = "Octez ${service-name}";
     environment = {
-      TEZOS_LOG = "* -> ${node-cfg.logVerbosity}";
+      OCTEZ_LOG = "* -> ${node-cfg.logVerbosity}";
     };
     serviceConfig = {
       User = "tezos-${node-name}";

nix/modules/tezos-node.nix
@@ -6,8 +6,8 @@
 with lib;
 
 let
-  tezos-node-pkg = pkgs.tezosPackages.tezos-node;
-  cfg = config.services.tezos-node;
+  octez-node-pkg = pkgs.octezPackages.octez-node;
+  cfg = config.services.octez-node;
   genConfigCommand = historyMode: rpcPort: netPort: network: options: ''
     --data-dir "$node_data_dir" \
     --history-mode "${historyMode}" \
@@ -18,10 +18,10 @@ let
   common = import ./common.nix { inherit lib; inherit pkgs; };
   instanceOptions = types.submodule ( {...} : {
     options = common.sharedOptions // {
-      enable = mkEnableOption "Tezos node service";
+      enable = mkEnableOption "Octez node service";
 
       package = mkOption {
-        default = tezos-node-pkg;
+        default = octez-node-pkg;
         type = types.package;
       };
 
@@ -30,7 +30,7 @@ let
         default = 8732;
         example = 8732;
         description = ''
-          Tezos node RPC port.
+          Octez node RPC port.
         '';
       };
 
@@ -39,13 +39,13 @@ let
         default = 9732;
         example = 9732;
         description = ''
-          Tezos node net port.
+          Octez node net port.
         '';
       };
 
       network = mkOption {
         type = types.str;
-        default = "jakartanet";
+        default = "kathmandunet";
         description = ''
           Network which node will be running on.
           Can be either a predefined network name or a URL to the network config.
@@ -65,7 +65,7 @@ let
         type = types.listOf types.str;
         default = [];
         description = ''
-          Additional 'tezos-node' options that affect configuration file.
+          Additional 'octez-node' options that affect configuration file.
         '';
       };
 
@@ -75,14 +75,14 @@ let
         description = ''
           Custom node config.
           This option overrides the all other options that affect
-          tezos-node config.
+          octez-node config.
         '';
       };
     };
   });
 in {
 
-  options.services.tezos-node = {
+  options.services.octez-node = {
     instances = mkOption {
       type = types.attrsOf instanceOptions;
       description = "Configuration options";
@@ -92,7 +92,7 @@ in {
   config = mkIf (cfg.instances != {}) {
     users = mkMerge (flip mapAttrsToList cfg.instances (node-name: node-cfg: common.genUsers node-name ));
     systemd = mkMerge (flip mapAttrsToList cfg.instances (node-name: node-cfg: {
-      services."tezos-${node-name}-tezos-node" = common.genSystemdService node-name node-cfg "node" // {
+      services."tezos-${node-name}-octez-node" = common.genSystemdService node-name node-cfg "node" // {
         after = [ "network.target" ];
         preStart =
           ''
@@ -104,10 +104,10 @@ in {
               ''
                 # Generate or update node config file
                 if [[ ! -f "$node_data_dir/config.json" ]]; then
-                  ${node-cfg.package}/bin/tezos-node config init \
+                  ${node-cfg.package}/bin/octez-node config init \
                   ${genConfigCommand node-cfg.historyMode node-cfg.rpcPort node-cfg.netPort node-cfg.network node-cfg.additionalOptions}
                 else
-                  ${node-cfg.package}/bin/tezos-node config update \
+                  ${node-cfg.package}/bin/octez-node config update \
                   ${genConfigCommand node-cfg.historyMode node-cfg.rpcPort node-cfg.netPort node-cfg.network node-cfg.additionalOptions}
                 fi
               ''
@@ -117,7 +117,7 @@ in {
               ''
           );
         script = ''
-          ${node-cfg.package}/bin/tezos-node run --data-dir "$STATE_DIRECTORY/node/data"
+          ${node-cfg.package}/bin/octez-node run --data-dir "$STATE_DIRECTORY/node/data"
         '';
       };
     }));

nix/modules/tezos-signer.nix
@@ -7,13 +7,13 @@
 with lib;
 
 let
-  tezos-signer-launch = "${pkgs.tezosPackages.tezos-signer}/bin/tezos-signer launch";
+  octez-signer-launch = "${pkgs.octezPackages.octez-signer}/bin/octez-signer launch";
   common = import ./common.nix { inherit lib; inherit pkgs; };
-  cfg = config.services.tezos-signer;
+  cfg = config.services.octez-signer;
   instanceOptions = types.submodule ( {...} : {
     options = common.sharedOptions // {
 
-      enable = mkEnableOption "Tezos signer service";
+      enable = mkEnableOption "Octez signer service";
 
       networkProtocol = mkOption {
         type = types.enum [ "http" "https" "tcp" "unix" ];
@@ -28,7 +28,7 @@ let
         default = "127.0.0.1";
         example = "127.0.0.1";
         description = ''
-          Tezos signer net address.
+          Octez signer net address.
         '';
       };
 
@@ -37,7 +37,7 @@ let
         default = 8080;
         example = 8080;
         description = ''
-          Tezos signer net port.
+          Octez signer net port.
         '';
       };
 
@@ -45,7 +45,7 @@ let
         type = types.str;
         default = null;
         description = ''
-          Path of the SSL certificate to use for https Tezos signer.
+          Path of the SSL certificate to use for https Octez signer.
         '';
       };
 
@@ -53,7 +53,7 @@ let
         type = types.str;
         default = null;
         description = ''
-          Key path to use for https Tezos signer.
+          Key path to use for https Octez signer.
         '';
       };
 
@@ -61,7 +61,7 @@ let
         type = types.str;
         default = null;
         description = ''
-          Socket to use for Tezos signer running over UNIX socket.
+          Socket to use for Octez signer running over UNIX socket.
         '';
       };
 
@@ -70,14 +70,14 @@ let
         default = 1;
         example = 1;
         description = ''
-          Timeout for Tezos signer.
+          Timeout for Octez signer.
         '';
       };
 
     };
   });
 in {
-  options.services.tezos-signer = {
+  options.services.octez-signer = {
     instances = mkOption {
       type = types.attrsOf instanceOptions;
       description = "Configuration options";
@@ -87,21 +87,21 @@ in {
   config = mkIf (cfg.instances != {}) {
     users = mkMerge (flip mapAttrsToList cfg.instances (node-name: node-cfg: common.genUsers node-name ));
     systemd = mkMerge (flip mapAttrsToList cfg.instances (node-name: node-cfg:
-      let tezos-signers = {
+      let octez-signers = {
         "http" =
-          "${tezos-signer-launch} http signer --address ${node-cfg.netAddress} --port ${toString node-cfg.netPort}";
+          "${octez-signer-launch} http signer --address ${node-cfg.netAddress} --port ${toString node-cfg.netPort}";
         "https" =
-          "${tezos-signer-launch} https signer ${node-cfg.certPath} ${node-cfg.keyPath} --address ${node-cfg.netAddress} --port ${toString node-cfg.netPort}";
+          "${octez-signer-launch} https signer ${node-cfg.certPath} ${node-cfg.keyPath} --address ${node-cfg.netAddress} --port ${toString node-cfg.netPort}";
         "tcp" =
-          "${tezos-signer-launch} socket signer --address ${node-cfg.netAddress} --port ${toString node-cfg.netPort} --timeout ${toString node-cfg.timeout}";
+          "${octez-signer-launch} socket signer --address ${node-cfg.netAddress} --port ${toString node-cfg.netPort} --timeout ${toString node-cfg.timeout}";
         "unix" =
-          "${tezos-signer-launch} local signer --socket ${node-cfg.unixSocket}";
+          "${octez-signer-launch} local signer --socket ${node-cfg.unixSocket}";
       };
       in {
-      services."tezos-${node-name}-tezos-signer" = common.genSystemdService node-name node-cfg "signer" // {
+      services."tezos-${node-name}-octez-signer" = common.genSystemdService node-name node-cfg "signer" // {
         after = [ "network.target" ];
         script = ''
-          ${tezos-signers.${node-cfg.networkProtocol}}
+          ${octez-signers.${node-cfg.networkProtocol}}
         '';
       };
     }));

tests/tezos-modules.nix
@@ -17,7 +17,7 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
                 ../nix/modules/tezos-baker.nix
               ];
 
-    services.tezos-node.instances.jakartanet = {
+    services.octez-node.instances.kathmandunet = {
       enable = true;
       additionalOptions = [
         "--bootstrap-threshold=1"
@@ -25,19 +25,19 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
       ];
     };
 
-    services.tezos-signer.instances.jakartanet = {
+    services.octez-signer.instances.kathmandunet = {
       enable = true;
       networkProtocol = "http";
     };
 
-    services.tezos-accuser.instances.jakartanet = {
+    services.octez-accuser.instances.kathmandunet = {
       enable = true;
-      baseProtocols = ["013-PtJakart" "014-PtKathma"];
+      baseProtocols = ["PtKathma"];
     };
 
-    services.tezos-baker.instances.jakartanet = {
+    services.octez-baker.instances.kathmandunet = {
       enable = true;
-      baseProtocols = ["013-PtJakart" "014-PtKathma"];
+      baseProtocols = ["PtKathma"];
       bakerAccountAlias = "baker";
       bakerSecretKey = "unencrypted:edsk3KaTNj1d8Xd3kMBrZkJrfkqsz4XwwiBXatuuVgTdPye2KpE98o";
     };
@@ -48,16 +48,16 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
     start_all()
 
     services = [
-        "tezos-node",
-        "tezos-signer",
-        "tezos-baker",
-        "tezos-accuser",
+        "octez-node",
+        "octez-signer",
+        "octez-baker",
+        "octez-accuser",
     ]
 
     for s in services:
-        machine.wait_for_unit(f"tezos-jakartanet-{s}.service")
+        machine.wait_for_unit(f"tezos-kathmandunet-{s}.service")
 
-    with subtest("check tezos-node rpc response"):
+    with subtest("check octez-node rpc response"):
         machine.wait_for_open_port(8732)
         machine.wait_until_succeeds(
             "curl --silent http://localhost:8732/chains/main/blocks/head/header | grep level"
@@ -65,6 +65,6 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
 
     with subtest("service status sanity check"):
         for s in services:
-            machine.succeed(f"systemctl status tezos-jakartanet-{s}.service")
+            machine.succeed(f"systemctl status tezos-kathmandunet-{s}.service")
   '';
 }) { inherit pkgs system; }
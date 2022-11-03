d4d92c493d01bfe9b637a7cb23b3e7f4f51efb4c
Dmitry Laptov <dmitry.laptov@serokell.io>
2022-11-03 16:03:31 +0300

    [Chore] Reset letter version
    Problem: New upstream release came and
    letter version shoud be reset.
    
    Solution: Reset it.

--- docker/package/model.py
+++ docker/package/model.py
@@ -519,7 +519,7 @@ class TezosBakingServicesPackage(AbstractPackage):
     # native releases, so we append an extra letter to the version of
     # the package.
     # This should be reset to "" whenever the native version is bumped.
-    letter_version = "a"
+    letter_version = ""
 
     buildfile = "setup.py"
 

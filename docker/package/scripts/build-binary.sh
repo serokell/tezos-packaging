e27416f7f5f6552e6126f008c13c9285c7cb7244
Dmitry Laptov <dmitry.laptov@serokell.io>
2022-12-05 22:08:00 +0300

    [Chore] Exclude dev deps for native packaging
    Problem: v15.1 octez release come with the dev deps opam file
    which is redundant and breaks installation process.
    
    Solution: Doesn't provide `octez-dev-deps.opam` as dependency to install.

--- docker/package/scripts/build-binary.sh
+++ docker/package/scripts/build-binary.sh
@@ -17,7 +17,10 @@ opam switch create . --repositories=local --no-install
 eval "$(opam env)"
 opams=()
 while IFS=  read -r -d $'\0'; do
-    opams+=("$REPLY")
+    # we exclude optional development packages
+    if [ "$REPLY" != "./opam/virtual/octez-dev-deps.opam" ]; then
+        opams+=("$REPLY")
+    fi
 done < <(find ./vendors ./src ./tezt ./opam -name \*.opam -print0)
 export CFLAGS="-fPIC ${CFLAGS:-}"
 opam install "${opams[@]}" --deps-only --criteria="-notuptodate,-changed,-removed"

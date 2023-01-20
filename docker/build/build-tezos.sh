3d6182f5c0afe082aa4d757fc87dc635e422a9a5
Savely Krendelhoff <alicecandyom@proton.me>
2023-01-20 15:40:22 +0300

    [Chore] Optimize static binaries build
    Problem: Separate steps are contained in `build-tezos.sh`,
    so if at some point script fails, everything will be started from
    scratch.
    
    Solution: Put fetching sources, building deps and building binaries
    steps on different layers to cache intermediary results.

--- docker/build/build-tezos.sh
+++ docker/build/build-tezos.sh
@@ -4,20 +4,9 @@
 # SPDX-License-Identifier: LicenseRef-MIT-OA
 
 # This script builds static tezos binaries. It expects patches required for static building to be
-# in parent directory, it also accepts tezos version as an argument.
+# in parent directory.
 set -euo pipefail
 
-tezos_version="$1"
-git clone --single-branch --branch "$tezos_version" https://gitlab.com/tezos/tezos.git --depth 1
-cd tezos
-
-source "$HOME/.cargo/env"
-export OPAMYES="true"
-# Disable usage of instructions from the ADX extension to avoid incompatibility
-# with old CPUs, see https://gitlab.com/dannywillems/ocaml-bls12-381/-/merge_requests/135/
-export BLST_PORTABLE="yes"
-opam init --bare --disable-sandboxing
-make build-deps
-
-eval "$(opam env)" && make static
+eval "$(opam env)"
+make static
 chmod +w octez-*

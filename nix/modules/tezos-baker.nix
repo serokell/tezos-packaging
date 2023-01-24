5788ed87a08b06cad23052b282854f34270f129c
Roman Melnikov <roman.melnikov@serokell.io>
2023-01-24 16:06:11 +0800

    [Chore] Support 'PtMumbai' in NixOS modules
    Problem: 'PtMumbai' was supported everywhere except for NixOS modules.
    
    Solution: Support 'PtMumbai' in NixOS modules as well.

--- nix/modules/tezos-baker.nix
+++ nix/modules/tezos-baker.nix
@@ -9,6 +9,8 @@ let
   octez-baker-pkgs = {
     "PtLimaPt" =
       "${pkgs.octezPackages.octez-baker-PtLimaPt}/bin/octez-baker-PtLimaPt";
+    "PtMumbai" =
+      "${pkgs.octezPackages.octez-baker-PtMumbai}/bin/octez-baker-PtMumbai";
   };
   octez-client = "${pkgs.octezPackages.octez-client}/bin/octez-client";
   cfg = config.services.octez-baker;

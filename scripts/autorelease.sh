c0aaf70f27dbd2e3be48d7f0724cec42e79cdd15
pasqu4le <pasqu4le@gmail.com>
2023-02-23 21:46:48 +0100

    sign arm static binaries in release

--- scripts/autorelease.sh
+++ scripts/autorelease.sh
@@ -3,12 +3,7 @@
 # SPDX-FileCopyrightText: 2021 Oxhead Alpha
 # SPDX-License-Identifier: LicenseRef-MIT-OA
 
-# This script takes the Buildkite build message, usually the last commit's title.
-
 set -euo pipefail
-
-# Project name, inferred from repository name
-project=$(basename "$(pwd)")
 
 # The directory in which artifacts will be created
 TEMPDIR=$(mktemp -d)
@@ -19,13 +14,8 @@ trap finish EXIT
 
 assets_dir=$TEMPDIR/assets
 
-# Build tezos-release
-nix-build . -A release -o "$TEMPDIR"/"$project" \
-          --arg docker-binaries ./binaries/docker --arg docker-arm-binaries ./arm-binaries/docker
-mkdir -p "$assets_dir"
-# Move archive with binaries and tezos license to assets
-shopt -s extglob
-cp -L "$TEMPDIR"/"$project"/!(*.md) "$assets_dir"
+gh release download "v16.0-rc3-1" -D "$assets_dir" -p "*-arm64"
+
 # Iterate over assets, calculate sha256sum and sign them in order
 # to include this additional info to the release assets as well
 for asset in "$assets_dir"/*; do
@@ -33,34 +23,5 @@ for asset in "$assets_dir"/*; do
     gpg --armor --detach-sign "$asset"
 done
 
-msg_regex="Merge pull request .* from serokell\/auto\/(v.*)-release"
-rc_regex="^v[0-9]+\.[0-9]+-rc[0-9]+"
-
-# We create a pre-release in any case except if we are merging a stable version release PR
-mode_flag="--prerelease"
-
-if [[ ${1-""} =~ $msg_regex ]]; then
-    tag="${BASH_REMATCH[1]}-$(jq -r '.release' meta.json)"
-    if [[ ! $tag =~ $rc_regex ]]; then
-        mode_flag=""
-    fi
-else
-    tag="auto-release"
-
-    # Delete autorelease if it exists
-    gh release delete auto-release --yes || true
-fi
-
-# Update the tag
-git fetch # So that the script can be run from an arbitrary checkout
-git tag -f "$tag"
-git push origin "$tag" --force
-
-# Create release
-# Note: "mode_flag" should not be quoted here because an empty value results in
-# two consecutive spaces, which gh will interpret incorrectly as an empty param
-# shellcheck disable=SC2086
-gh release create "$tag" --title "$tag" $mode_flag -F "$TEMPDIR"/"$project"/release-notes.md
-
 # Upload assets
-gh release upload "$tag" "$assets_dir"/*
+gh release upload "v16.0-rc3-1" "$assets_dir"/* --clobber

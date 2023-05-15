b21abc017c00ff9cb1db11215e75ae52e860cd98
Savely Krendelhoff <alicecandyom@proton.me>
2023-05-15 16:14:39 +0700

    [#646] Consider enable option in modules
    Problem: if you attempt to define a configuration using NixOS modules
    but decided to disable it, it will still attempt to start module.
    
    Solution: Do not run service on startup if it is disabled and
    tweak test script according to services' enable option.

--- tests/tezos-modules.nix
+++ tests/tezos-modules.nix
@@ -3,6 +3,31 @@
 { nixpkgs, pkgs, ... }:
 let
   inherit (pkgs) system;
+
+  octez-node = {
+    enable = true;
+    additionalOptions = [
+      "--bootstrap-threshold=1"
+      "--connections" "50"
+    ];
+  };
+
+  octez-signer = {
+    enable = true;
+    networkProtocol = "http";
+  };
+
+  octez-accuser = {
+    enable = true;
+    baseProtocols = ["PtMumbai"];
+  };
+
+  octez-baker = {
+    enable = true;
+    baseProtocols = ["PtMumbai"];
+    bakerAccountAlias = "baker";
+    bakerSecretKey = "unencrypted:edsk3KaTNj1d8Xd3kMBrZkJrfkqsz4XwwiBXatuuVgTdPye2KpE98o";
+  };
 in
 import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
 {
@@ -18,51 +43,38 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
                 ../nix/modules/tezos-baker.nix
               ];
 
-    services.octez-node.instances.mumbainet = {
-      enable = true;
-      additionalOptions = [
-        "--bootstrap-threshold=1"
-        "--connections" "50"
-      ];
-    };
-
-    services.octez-signer.instances.mumbainet = {
-      enable = true;
-      networkProtocol = "http";
-    };
-
-    services.octez-accuser.instances.mumbainet = {
-      enable = true;
-      baseProtocols = ["PtMumbai"];
-    };
-
-    services.octez-baker.instances.mumbainet = {
-      enable = true;
-      baseProtocols = ["PtMumbai"];
-      bakerAccountAlias = "baker";
-      bakerSecretKey = "unencrypted:edsk3KaTNj1d8Xd3kMBrZkJrfkqsz4XwwiBXatuuVgTdPye2KpE98o";
+    services = {
+      octez-node.instances.mumbainet = octez-node;
+      octez-signer.instances.mumbainet = octez-signer;
+      octez-accuser.instances.mumbainet = octez-accuser;
+      octez-baker.instances.mumbainet = octez-baker;
     };
 
   };
 
   testScript = ''
+    from typing import List
+
     start_all()
 
-    services = [
-        "octez-node",
-        "octez-signer",
-        "octez-baker",
-        "octez-accuser",
+    services: List[str] = [
+        ${if octez-node.enable then ''"octez-node",'' else ""}
+        ${if octez-signer.enable then ''"octez-signer",'' else ""}
+        ${if octez-accuser.enable then ''"octez-accuser",'' else ""}
+        ${if octez-baker.enable then ''"octez-baker",'' else ""}
     ]
 
     for s in services:
         machine.wait_for_unit(f"tezos-mumbainet-{s}.service")
 
+    ${if octez-node.enable then ''
     with subtest("check octez-node rpc response"):
         machine.wait_for_open_port(8732)
         machine.wait_until_succeeds(
             "curl --silent http://localhost:8732/chains/main/blocks/head/header | grep level"
         )
+    '' else ""}
+
 
     with subtest("service status sanity check"):
         for s in services:

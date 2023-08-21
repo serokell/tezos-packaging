serokell/tezos-packaging-rc

[Chore] Remove PtMumbai and mumbainet support
Problem: PtMumbai protocol is deprecated.

Solution: Remove it support.

nix/modules/tezos-node.nix
@@ -45,7 +45,7 @@ let
 
       network = mkOption {
         type = types.str;
-        default = "mumbainet";
+        default = "nairobinet";
         description = ''
           Network which node will be running on.
           Can be either a predefined network name or a URL to the network config.

tests/test_script.py
@@ -77,8 +77,8 @@ def test_node_with_daemons_scenario(network, use_tls=False):
     kill_node_with_daemons()
 
 
-with subtest("run node with daemons on mumbainet"):
-    test_node_with_daemons_scenario("mumbainet")
+with subtest("run node with daemons on nairobinet"):
+    test_node_with_daemons_scenario("nairobinet")
 
 with subtest("run node with daemons on mainnet"):
     test_node_with_daemons_scenario("mainnet")

tests/tezos-modules.nix
@@ -19,12 +19,12 @@ let
 
   octez-accuser = {
     enable = true;
-    baseProtocols = ["PtMumbai"];
+    baseProtocols = ["PtNairob"];
   };
 
   octez-baker = {
     enable = true;
-    baseProtocols = ["PtMumbai"];
+    baseProtocols = ["PtNairob"];
     bakerAccountAlias = "baker";
     bakerSecretKey = "unencrypted:edsk3KaTNj1d8Xd3kMBrZkJrfkqsz4XwwiBXatuuVgTdPye2KpE98o";
   };
@@ -44,10 +44,10 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
               ];
 
     services = {
-      octez-node.instances.mumbainet = octez-node;
-      octez-signer.instances.mumbainet = octez-signer;
-      octez-accuser.instances.mumbainet = octez-accuser;
-      octez-baker.instances.mumbainet = octez-baker;
+      octez-node.instances.nairobinet = octez-node;
+      octez-signer.instances.nairobinet = octez-signer;
+      octez-accuser.instances.nairobinet = octez-accuser;
+      octez-baker.instances.nairobinet = octez-baker;
     };
 
   };
@@ -65,7 +65,7 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
     ]
 
     for s in services:
-        machine.wait_for_unit(f"tezos-mumbainet-{s}.service")
+        machine.wait_for_unit(f"tezos-nairobinet-{s}.service")
 
     ${if octez-node.enable then ''
     with subtest("check octez-node rpc response"):
@@ -78,6 +78,6 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
 
     with subtest("service status sanity check"):
         for s in services:
-            machine.succeed(f"systemctl status tezos-mumbainet-{s}.service")
+            machine.succeed(f"systemctl status tezos-nairobinet-{s}.service")
   '';
 }) { inherit pkgs system; }

serokell/tezos-packaging

nix/modules/tezos-node.nix
@@ -45,7 +45,7 @@ let
 
       network = mkOption {
         type = types.str;
-        default = "mumbainet";
+        default = "ghostnet";
         description = ''
           Network which node will be running on.
           Can be either a predefined network name or a URL to the network config.

tests/test_script.py
@@ -77,8 +77,8 @@ def test_node_with_daemons_scenario(network, use_tls=False):
     kill_node_with_daemons()
 
 
-with subtest("run node with daemons on mumbainet"):
-    test_node_with_daemons_scenario("mumbainet")
+with subtest("run node with daemons on ghostnet"):
+    test_node_with_daemons_scenario("ghostnet")
 
 with subtest("run node with daemons on mainnet"):
     test_node_with_daemons_scenario("mainnet")

tests/tezos-modules.nix
@@ -19,12 +19,12 @@ let
 
   octez-accuser = {
     enable = true;
-    baseProtocols = ["PtMumbai"];
+    baseProtocols = ["PtNairob"];
   };
 
   octez-baker = {
     enable = true;
-    baseProtocols = ["PtMumbai"];
+    baseProtocols = ["PtNairob"];
     bakerAccountAlias = "baker";
     bakerSecretKey = "unencrypted:edsk3KaTNj1d8Xd3kMBrZkJrfkqsz4XwwiBXatuuVgTdPye2KpE98o";
   };
@@ -44,10 +44,10 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
               ];
 
     services = {
-      octez-node.instances.mumbainet = octez-node;
-      octez-signer.instances.mumbainet = octez-signer;
-      octez-accuser.instances.mumbainet = octez-accuser;
-      octez-baker.instances.mumbainet = octez-baker;
+      octez-node.instances.ghostnet = octez-node;
+      octez-signer.instances.ghostnet = octez-signer;
+      octez-accuser.instances.ghostnet = octez-accuser;
+      octez-baker.instances.ghostnet = octez-baker;
     };
 
   };
@@ -65,7 +65,7 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
     ]
 
     for s in services:
-        machine.wait_for_unit(f"tezos-mumbainet-{s}.service")
+        machine.wait_for_unit(f"tezos-ghostnet-{s}.service")
 
     ${if octez-node.enable then ''
     with subtest("check octez-node rpc response"):
@@ -78,6 +78,6 @@ import "${nixpkgs}/nixos/tests/make-test-python.nix" ({ ... }:
 
     with subtest("service status sanity check"):
         for s in services:
-            machine.succeed(f"systemctl status tezos-mumbainet-{s}.service")
+            machine.succeed(f"systemctl status tezos-ghostnet-{s}.service")
   '';
 }) { inherit pkgs system; }
serokell/tezos-packaging-stable

[#102] Update documetnation on tezos binaries
Problem: We need to update documentation for tezos-binaries

Solution: updated documentation, adding links to the corresponding docs

nix/build/release-binaries.nix
@@ -8,7 +8,10 @@ let
 in [
   {
     name = "octez-client";
-    description = "CLI client for interacting with octez blockchain";
+    description = ''
+      CLI client for interacting with octez blockchain and a basic wallet.
+      For more information see - http://tezos.gitlab.io/introduction/howtouse.html#client
+    '';
     supports = protocolsFormatted;
   }
   {
@@ -18,55 +21,83 @@ in [
   }
   {
     name = "octez-admin-client";
-    description = "Administration tool for the node";
+    description = ''
+      CLI adminitrator tool with additional possibilities.
+      For more information please check - http://tezos.gitlab.io/user/various.html#octez-admin-client
+    '';
     supports = protocolsFormatted;
   }
   {
     name = "octez-node";
-    description =
-      "Entry point for initializing, configuring and running a Octez node";
+    description = ''
+      Entry point for initializing, configuring and running a Octez node
+      For more information please check - http://tezos.gitlab.io/introduction/howtouse.html#node
+    '';
     supports = protocolsFormatted;
   }
   {
     name = "octez-dac-node";
-    description =
-      "A Data Availability Committee Tezos node";
+    description =''
+      A Data Availability Committee Tezos node.
+      For more info on DAC please check https://research-development.nomadic-labs.com/introducing-data-availability-committees.html
+    '';
     supports = protocolsFormatted;
   }
   {
     name = "octez-signer";
-    description = "A client to remotely sign operations or blocks";
+    description = ''
+      A client to remotely sign operations or blocks.
+      For more info please check - http://tezos.gitlab.io/user/key-management.html#signer
+    '';
     supports = protocolsFormatted;
   }
   {
     name = "octez-codec";
-    description = "A client to decode and encode JSON";
+    description = ''
+      A utility for documenting the data encodings and for performing data encoding/decoding.
+      For more info please check - http://tezos.gitlab.io/introduction/howtouse.html#codec
+    '';
     supports = protocolsFormatted;
   }
   {
     name = "octez-smart-rollup-wasm-debugger";
-    description = "Smart contract rollup wasm debugger";
+    description = ''
+      Debugger for smart rollup kernels
+      For more info please check - http://tezos.gitlab.io/active/smart_rollups.html
+    '';
     supports = protocolsFormatted;
   }
 ] ++ builtins.concatMap (protocol: [
   {
     name = "octez-baker-${protocol}";
-    description = "Daemon for baking";
+    description = ''
+      Daemon for baking for ${protocol} protocol.
+      For more info please check - http://tezos.gitlab.io/introduction/howtorun.html#baker
+    '';
     supports = protocol;
   }
   {
     name = "octez-accuser-${protocol}";
-    description = "Daemon for accusing";
+    description = ''
+      Daemon for accusing for ${protocol} protocol.
+      For more info please check - http://tezos.gitlab.io/introduction/howtorun.html#accuser
+    '';
     supports = protocol;
   }
   {
     name = "octez-smart-rollup-client-${protocol}";
-    description = "Smart contract rollup CLI client for ${protocol}";
+    description = ''
+      Smart contract rollup CLI client for ${protocol}.
+      For more info please check - http://tezos.gitlab.io/active/smart_rollups.html#tools
+    '';
     supports = protocol;
   }
   {
     name = "octez-smart-rollup-node-${protocol}";
-    description = "Tezos smart contract rollup node for ${protocol}";
+    description = ''
+      Tezos smart contract rollup node for ${protocol}.
+      For more info please check - http://tezos.gitlab.io/active/smart_rollups.html#tools
+    '';
     supports = protocol;
   }
 ]) protocols.active

serokell/tezos-packaging

nix/build/release-binaries.nix
@@ -8,7 +8,10 @@ let
 in [
   {
     name = "octez-client";
-    description = "CLI client for interacting with octez blockchain";
+    description = ''
+      CLI client for interacting with octez blockchain and a basic wallet.
+      For more information see - https://tezos.gitlab.io/introduction/howtouse.html#client
+    '';
     supports = protocolsFormatted;
   }
   {
@@ -18,55 +21,83 @@ in [
   }
   {
     name = "octez-admin-client";
-    description = "Administration tool for the node";
+    description = ''
+      CLI administrator tool for peers management
+      For more information please check - https://tezos.gitlab.io/user/various.html#octez-admin-client
+    '';
     supports = protocolsFormatted;
   }
   {
     name = "octez-node";
-    description =
-      "Entry point for initializing, configuring and running a Octez node";
+    description = ''
+      Entry point for initializing, configuring and running a Octez node
+      For more information please check - https://tezos.gitlab.io/introduction/howtouse.html#node
+    '';
     supports = protocolsFormatted;
   }
   {
     name = "octez-dac-node";
-    description =
-      "A Data Availability Committee Tezos node";
+    description =''
+      A Data Availability Committee Tezos node.
+      For more info on DAC please check https://research-development.nomadic-labs.com/introducing-data-availability-committees.html
+    '';
     supports = protocolsFormatted;
   }
   {
     name = "octez-signer";
-    description = "A client to remotely sign operations or blocks";
+    description = ''
+      A client to remotely sign operations or blocks.
+      For more info please check - https://tezos.gitlab.io/user/key-management.html#signer
+    '';
     supports = protocolsFormatted;
   }
   {
     name = "octez-codec";
-    description = "A client to decode and encode JSON";
+    description = ''
+      A utility for documenting the data encodings and for performing data encoding/decoding.
+      For more info please check - https://tezos.gitlab.io/introduction/howtouse.html#codec
+    '';
     supports = protocolsFormatted;
   }
   {
     name = "octez-smart-rollup-wasm-debugger";
-    description = "Smart contract rollup wasm debugger";
+    description = ''
+      Debugger for smart rollup kernels
+      For more info please check - https://tezos.gitlab.io/active/smart_rollups.html
+    '';
     supports = protocolsFormatted;
   }
 ] ++ builtins.concatMap (protocol: [
   {
     name = "octez-baker-${protocol}";
-    description = "Daemon for baking";
+    description = ''
+      Daemon for baking for ${protocol} protocol.
+      For more info please check - https://tezos.gitlab.io/introduction/howtorun.html#baker
+    '';
     supports = protocol;
   }
   {
     name = "octez-accuser-${protocol}";
-    description = "Daemon for accusing";
+    description = ''
+      Daemon for accusing for ${protocol} protocol.
+      For more info please check - https://tezos.gitlab.io/introduction/howtorun.html#accuser
+    '';
     supports = protocol;
   }
   {
     name = "octez-smart-rollup-client-${protocol}";
-    description = "Smart contract rollup CLI client for ${protocol}";
+    description = ''
+      Smart contract rollup CLI client for ${protocol}.
+      For more info please check - https://tezos.gitlab.io/active/smart_rollups.html#tools
+    '';
     supports = protocol;
   }
   {
     name = "octez-smart-rollup-node-${protocol}";
-    description = "Tezos smart contract rollup node for ${protocol}";
+    description = ''
+      Tezos smart contract rollup node for ${protocol}.
+      For more info please check - https://tezos.gitlab.io/active/smart_rollups.html#tools
+    '';
     supports = protocol;
   }
 ]) protocols.active
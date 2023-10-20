76b1cd5f5de552c2f55b9f79bbb43cfe9b67b28c
Savely Krendelhoff <alicecandyom@proton.me>
2023-10-20 19:27:11 +0300

    [#735] Set BLST_PORTABLE env var on correct stage
    Problem: Since
    [!8631](https://gitlab.com/tezos/tezos/-/merge_requests/8631),
    ocaml-bls12-381 library was moved into tezos/tezos tree, so
    BLST_PORTABLE set for deps build was ignored.
    
    Solution: Set it for tezos build.

--- docker/build/build-deps.sh
+++ docker/build/build-deps.sh
@@ -6,8 +6,5 @@
 set -euo pipefail
 
 source "$HOME/.cargo/env"
-# Disable usage of instructions from the ADX extension to avoid incompatibility
-# with old CPUs, see https://gitlab.com/dannywillems/ocaml-bls12-381/-/merge_requests/135/
-export BLST_PORTABLE="yes"
 opam init --bare --disable-sandboxing
 make build-deps

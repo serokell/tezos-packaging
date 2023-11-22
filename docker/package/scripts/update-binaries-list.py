4b61b1a822454d59870dbb953eb47a1cc5ce9980
PruStephan <prustephan98@gmail.com>
2023-11-22 23:58:48 +0100

    [#736] Add setup to automatically update binaries
    Problem: We want to automatically update list of binaries after a tag build
    
    Soltuion: Add setup to update list of binaries and create a followup pr in case of new release. Note that due to how it works, the first PR will probably contain a lot of changes to the list (because the list is sorted in a different order from what buildkite artifacts provide).

---
+++ docker/package/scripts/update-binaries-list.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import json
+
+binaries_json_path_suffix = "/tests/binaries.json"
+binaries_list_path_suffix = "/tmp/binaries.txt"
+
+def update_binaries(binaries, field):
+    binaries_json_path = os.environ["PWD"] + binaries_json_path_suffix
+    with open(binaries_json_path, 'r') as file:
+        data = json.load(file)
+
+    data[field] = binaries
+    with open(binaries_json_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+def main():
+    tag = os.environ["BUILDKITE_TAG"]
+    binaries = []
+
+    binaries_list_path = os.environ["PWD"] + binaries_list_path_suffix
+    with open(binaries_list_path, 'r') as f:
+        binaries = [l.strip() for l in f.readlines()]
+
+    if not binaries:
+        raise Exception('Exception, while reading binaries list: binaries list is empty')
+
+    field = 'released'
+    if 'rc' in tag:
+        field = 'candidates'
+
+    update_binaries(binaries, field)
+    os.remove(binaries_list_path)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ docker/update-binaries.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2019 TQ Tezos <https://tqtezos.com/>
+#
+# SPDX-License-Identifier: LicenseRef-MIT-TQ
+
+bot_name="CI bot"
+branch="update-binaries-list-$BUILDKITE_TAG"
+
+git config --local user.email "hi@serokell.io"
+git config --local user.name "$bot_name"
+git fetch --all
+git checkout -B "$branch"
+
+python3 package/scripts/update-binaries-list.py
+
+git add --all
+git commit -m "Updated binaries for $BUILDKITE_TAG release" --gpg-sign="tezos-packaging@serokell.io"
+git push --set-upstream origin "$branch" --force-with-lease
+gh pr create -B master -t "Update list of binaries for $BUILDKITE_TAG" -F ../.github/binaries_update_pr.md

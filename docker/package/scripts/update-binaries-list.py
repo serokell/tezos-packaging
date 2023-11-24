44e8b02cb9352d820a23a89606297321e9c8ccc8
PruStephan <prustephan98@gmail.com>
2023-11-24 22:06:16 +0100

    fixup! [#736] Add setup to automatically update binaries

--- docker/package/scripts/update-binaries-list.py
+++ docker/package/scripts/update-binaries-list.py
@@ -5,36 +5,37 @@ import os
 import sys
 import json
 
-binaries_json_path_suffix = "/tests/binaries.json"
-binaries_list_path_suffix = "/tmp/binaries.txt"
+binaries_json_path_suffix = "tests/binaries.json"
+binaries_list_path_suffix = "tmp/binaries.txt"
 
 
 def update_binaries(binaries, field):
-    binaries_json_path = os.environ["PWD"] + binaries_json_path_suffix
+    binaries_json_path = os.path.join(os.environ["PWD"], binaries_json_path_suffix)
     with open(binaries_json_path, "r") as file:
         data = json.load(file)
 
     data[field] = binaries
     with open(binaries_json_path, "w") as file:
         json.dump(data, file, indent=4)
+        file.write("\n")
 
 
 def main():
     tag = os.environ["BUILDKITE_TAG"]
     binaries = []
 
-    binaries_list_path = os.environ["PWD"] + binaries_list_path_suffix
+    binaries_list_path = os.path.join(os.environ["PWD"], binaries_list_path_suffix)
     with open(binaries_list_path, "r") as f:
-        binaries = [l.strip() for l in f.readlines()]
+        binaries = [l.strip().replace("octez", "tezos") for l in f.readlines()]
+
+    binaries += ["tezos-baking", "tezos-sampling-params"]
 
     if not binaries:
         raise Exception(
             "Exception, while reading binaries list: binaries list is empty"
         )
 
-    field = "released"
-    if "rc" in tag:
-        field = "candidates"
+    field = "candidates" if "rc" in tag else "released"
 
     update_binaries(binaries, field)
     os.remove(binaries_list_path)
@@ -42,3 +43,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+

1095e7e321b59789a8071192126b74c675d8d183
krendelhoff2 <alicecandyom@proton.me>
2024-05-29 14:06:16 +0530

    [Chore] Support octez-dal-node

---
+++ Formula/tezos-dal-node.rb
@@ -0,0 +1,61 @@
+# SPDX-FileCopyrightText: 2024 Oxhead Alpha
+# SPDX-License-Identifier: LicenseRef-MIT-OA
+
+class TezosDalNode < Formula
+  @all_bins = []
+
+  class << self
+    attr_accessor :all_bins
+  end
+  homepage "https://gitlab.com/tezos/tezos"
+
+  url "https://gitlab.com/tezos/tezos.git", :tag => "octez-v20.0", :shallow => false
+
+  version "v20.0-1"
+
+  build_dependencies = %w[pkg-config coreutils autoconf rsync wget rustup-init cmake opam]
+  build_dependencies.each do |dependency|
+    depends_on dependency => :build
+  end
+
+  dependencies = %w[gmp hidapi libev protobuf sqlite tezos-sapling-params]
+  dependencies.each do |dependency|
+    depends_on dependency
+  end
+  desc "A Data Availability Layer Tezos node"
+
+  bottle do
+    root_url "https://github.com/serokell/tezos-packaging/releases/download/#{TezosDalNode.version}/"
+  end
+
+  def make_deps
+    ENV.deparallelize
+    ENV["CARGO_HOME"]="./.cargo"
+    # Disable usage of instructions from the ADX extension to avoid incompatibility
+    # with old CPUs, see https://gitlab.com/dannywillems/ocaml-bls12-381/-/merge_requests/135/
+    ENV["BLST_PORTABLE"]="yes"
+    # Force linker to use libraries from the current brew installation.
+    # Workaround for https://github.com/serokell/tezos-packaging/issues/700
+    ENV["LDFLAGS"] = "-L#{HOMEBREW_PREFIX}/lib"
+    # Here is the workaround to use opam 2.0.9 because Tezos is currently not compatible with opam 2.1.0 and newer
+    arch = RUBY_PLATFORM.include?("arm64") ? "arm64" : "x86_64"
+    system "rustup-init", "--default-toolchain", "1.71.1", "-y"
+    system "opam", "init", "--bare", "--debug", "--auto-setup", "--disable-sandboxing"
+    system ["source .cargo/env",  "make build-deps"].join(" && ")
+  end
+
+  def install_template(dune_path, exec_path, name)
+    bin.mkpath
+    self.class.all_bins << name
+    system ["eval $(opam env)", "dune build #{dune_path}", "cp #{exec_path} #{name}"].join(" && ")
+    bin.install name
+    ln_sf "#{bin}/#{name}", "#{bin}/#{name.gsub("octez", "tezos")}"
+  end
+
+  def install
+    make_deps
+    install_template "src/bin_dal_node/main.exe",
+                     "_build/default/src/bin_dal_node/main.exe",
+                     "octez-dal-node"
+  end
+end

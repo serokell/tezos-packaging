632f3ca74ea0349744cecd86e5db3ff8416c2978
krendelhoff2 <alicecandyom@proton.me>
2024-06-06 12:29:13 +0530

    [Chore] Bump release version

--- docker/package/model.py
+++ docker/package/model.py
@@ -585,7 +585,7 @@ class TezosBakingServicesPackage(AbstractPackage):
     # native releases, so we append an extra letter to the version of
     # the package.
     # This should be reset to "" whenever the native version is bumped.
-    letter_version = ""
+    letter_version = "a"
 
     buildfile = "setup.py"
 

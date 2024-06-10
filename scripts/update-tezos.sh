15c8bff951928e1beab2e01a6703ea45d174c462
krendelhoff2 <alicecandyom@proton.me>
2024-06-10 13:07:49 +0530

    [Chore] Fix octez tag handling
    Problem: new octez releases come with `octez-` prepended
    to the tag. It doesn't make sense for us to follow this
    style.
    
    Solution: Strip the prefix and leave only relevant part of
    tag for us.

--- scripts/update-tezos.sh
+++ scripts/update-tezos.sh
@@ -26,7 +26,9 @@ cp script-inputs/active_protocol_versions_without_number ../docker/active-protoc
 cd ..
 rm -rf upstream-repo
 
-branch_name="auto/$latest_upstream_tag-release"
+packaging_tag="$([[ "$latest_upstream_tag" =~ octez-(v.*) ]] && echo "${BASH_REMATCH[1]}")"
+
+branch_name="auto/$packaging_tag-release"
 
 our_tezos_tag="$(jq -r '.tezos_ref' meta.json | cut -d'/' -f3)"
 
@@ -38,37 +40,37 @@ if [[ "$latest_upstream_tag" != "$our_tezos_tag" ]]; then
   # wasn't created
   if ! git rev-parse --verify "$branch_name"; then
     git switch -c "$branch_name"
-    echo "Updating Tezos to $latest_upstream_tag"
+    echo "Updating Tezos to $packaging_tag"
 
     ./scripts/update-input.py tezos "$latest_upstream_tag_hash"
     ./scripts/update-input.py opam-repository "$full_opam_repository_tag"
-    git commit -a -m "[Chore] Bump Tezos sources to $latest_upstream_tag" --gpg-sign="tezos-packaging@serokell.io"
+    git commit -a -m "[Chore] Bump Tezos sources to $packaging_tag" --gpg-sign="tezos-packaging@serokell.io"
 
-    ./scripts/update-brew-formulae.sh "$latest_upstream_tag-1"
-    git commit -a -m "[Chore] Update brew formulae for $latest_upstream_tag" --gpg-sign="tezos-packaging@serokell.io"
+    ./scripts/update-brew-formulae.sh "$packaging_tag-1"
+    git commit -a -m "[Chore] Update brew formulae for $packaging_tag" --gpg-sign="tezos-packaging@serokell.io"
 
     sed -i 's/"release": "[0-9]\+"/"release": "1"/' ./meta.json
     # Update version of tezos-baking package
-    sed -i "s/version = .*/version = \"$latest_upstream_tag\"/" ./baking/pyproject.toml
+    sed -i "s/version = .*/version = \"$packaging_tag\"/" ./baking/pyproject.toml
     # Commit may fail when release number wasn't updated since the last release
-    git commit -a -m "[Chore] Reset release number for $latest_upstream_tag" --gpg-sign="tezos-packaging@serokell.io" || \
+    git commit -a -m "[Chore] Reset release number for $packaging_tag" --gpg-sign="tezos-packaging@serokell.io" || \
       (true; echo "release number wasn't updated")
 
     sed -i 's/letter_version *= *"[a-z]"/letter_version = ""/' ./docker/package/model.py
     # Commit may fail when the letter version wasn't updated since the last release
-    git commit -a -m "[Chore] Reset letter_version for $latest_upstream_tag" --gpg-sign="tezos-packaging@serokell.io" || \
+    git commit -a -m "[Chore] Reset letter_version for $packaging_tag" --gpg-sign="tezos-packaging@serokell.io" || \
       (true; echo "letter_version wasn't reset")
 
     ./scripts/update-release-binaries.py
     pushd docker
     python3 -m package.update-test-binaries-list
     popd
-    git commit -a -m "[Chore] Update release binaries for $latest_upstream_tag" --gpg-sign="tezos-packaging@serokell.io" || \
+    git commit -a -m "[Chore] Update release binaries for $packaging_tag" --gpg-sign="tezos-packaging@serokell.io" || \
       (true; echo "lists of binaries and protocols weren't updated")
 
     git push --set-upstream origin "$branch_name"
 
-    gh pr create -B master -t "[Chore] $latest_upstream_tag release" -F .github/release_pull_request_template.md
+    gh pr create -B master -t "[Chore] $packaging_tag release" -F .github/release_pull_request_template.md
   fi
 else
   echo "Our version is the same as the latest tag in the upstream repository"

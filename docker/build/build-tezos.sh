bfe00f5925adb6250c51e109878032d7bb56a234
krendelhoff2 <alicecandyom@proton.me>
2024-09-25 10:30:23 +0700

    [Chore] Bump alpine version and add deps

--- docker/build/build-tezos.sh
+++ docker/build/build-tezos.sh
@@ -11,5 +11,6 @@ eval "$(opam env)"
 # Disable usage of instructions from the ADX extension to avoid incompatibility
 # with old CPUs, see https://gitlab.com/dannywillems/ocaml-bls12-381/-/merge_requests/135/
 export BLST_PORTABLE="yes"
+
 make static
 chmod +w octez-*

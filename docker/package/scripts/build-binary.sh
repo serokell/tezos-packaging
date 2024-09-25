67b14f9cab799a553894387bf31c6588b5204a59
krendelhoff2 <alicecandyom@proton.me>
2024-09-25 12:09:48 +0700

    [Chore] Update native binary build script

--- docker/package/scripts/build-binary.sh
+++ docker/package/scripts/build-binary.sh
@@ -11,17 +11,27 @@ export OPAMROOT=$PWD/opamroot
 dune_filepath="$1"
 binary_name="$2"
 
+echo "******* Initializing opam-repository... *******"
+opam init tezos ./opam-repository --bare --disable-sandboxing
+
+echo "******* Entering tezos... *******"
 cd tezos
-opam init local ../opam-repository --bare --disable-sandboxing
-opam switch create . --repositories=local --no-install
+
+echo "******* Creating switch *******"
+
+. scripts/version.sh
+
+opam switch create . --repositories=tezos "ocaml-base-compiler.$ocaml_version" --no-install
+
+export OPAMSWITCH="$PWD"
+opam repository remove default > /dev/null 2>&1
 
 eval "$(opam env)"
 OPAMASSUMEDEPEXTS=true opam install conf-rust conf-rust-2021
 
 export CFLAGS="-fPIC ${CFLAGS:-}"
-OPAMASSUMEDEPEXTS=true opam install opam/virtual/octez-deps.opam --deps-only --criteria="-notuptodate,-changed,-removed"
+OPAMASSUMEDEPEXTS=true opam install opam/virtual/octez-deps.opam.locked --deps-only --criteria="-notuptodate,-changed,-removed"
 
-eval "$(opam env)"
 dune build "$dune_filepath"
 cp "./_build/default/$dune_filepath" "../$binary_name"
 cd ..

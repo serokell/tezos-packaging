b21d8096ebf5ab7a177781391ad18cb1b5531365
Savely Krendelhoff <alicecandyom@proton.me>
2025-02-27 17:07:00 +0700

    [Chore] Remove deprecated binaries

--- docker/package/packages.py
+++ docker/package/packages.py
@@ -178,22 +178,6 @@ packages = [
         )
     },
     {
-        "tezos-dac-client": TezosBinaryPackage(
-            "tezos-dac-client",
-            "A Data Availability Committee Tezos client",
-            meta=packages_meta,
-            dune_filepath="src/bin_dac_client/main_dac_client.exe",
-        )
-    },
-    {
-        "tezos-dac-node": TezosBinaryPackage(
-            "tezos-dac-node",
-            "A Data Availability Committee Tezos node",
-            meta=packages_meta,
-            dune_filepath="src/bin_dac_node/main_dac.exe",
-        )
-    },
-    {
         "tezos-dal-node": TezosBinaryPackage(
             "tezos-dal-node",
             "A Data Availability Layer Tezos node",
